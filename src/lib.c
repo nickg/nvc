e6a70f8d736b430e6d18c125a1e2767ca1ac9150
Nick Gasson <nick@nickg.me.uk>
2011-06-12 21:14:30 +0100

    Start testing lib.c

---
+++ src/lib.c
@@ -0,0 +1,6 @@
+#include "lib.h"
+
+lib_t make_lib(const char *name)
+{
+   return NULL;
+}
--- test/test_lib.c
+++ test/test_lib.c
@@ -1,16 +1,30 @@
 #include "lib.h"
 
 #include <check.h>
+#include <stdlib.h>
 
-START_TEST(test_name)
+START_TEST(test_make_lib)
 {
-   
+   lib_t l = make_lib("test");
+   fail_if(l == NULL);
 }
 END_TEST
 
-
 int main(void)
 {
-   return 1;
+   Suite *s = suite_create("lib");
+
+   TCase *tc_core = tcase_create("Core");
+   tcase_add_test(tc_core, test_make_lib);
+   suite_add_tcase(s, tc_core);
+   
+   SRunner *sr = srunner_create(s);
+   srunner_run_all(sr, CK_NORMAL);
+
+   int nfail = srunner_ntests_failed(sr);
+
+   srunner_free(sr);
+   
+   return nfail == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
