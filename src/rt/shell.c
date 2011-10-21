6dd8ac7451168309418a426bc0bbe2d2c25137ec
Nick Gasson <nick@nickg.me.uk>
2011-10-21 13:50:13 -0400

    Add show command to TCL prompt

--- src/rt/shell.c
+++ src/rt/shell.c
@@ -22,6 +22,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <tcl/tcl.h>
@@ -57,6 +58,42 @@ static int shell_cmd_quit(ClientData cd, Tcl_Interp *interp,
    slave_post_msg(SLAVE_QUIT, NULL, 0);
    bool *have_quit = (bool*)cd;
    *have_quit = true;
+   return TCL_OK;
+}
+
+static int shell_cmd_show(ClientData cd, Tcl_Interp *interp,
+                          int objc, Tcl_Obj *const objv[])
+{
+   tree_t top = cd;
+
+   if (objc == 1) {
+      fprintf(stderr, "try 'show help' for usage\n");
+      return TCL_ERROR;
+   }
+
+   const char *what = Tcl_GetString(objv[1]);
+   if (strcmp(what, "help") == 0) {
+      printf("Usage: show [something]\n"
+             "  signals - list all signals in design with current value\n"
+             "  process - list all processes in design\n");
+   }
+   else if (strcmp(what, "signals") == 0) {
+      for (unsigned i = 0; i < tree_decls(top); i++) {
+         tree_t d = tree_decl(top, i);
+         printf("%-30s%s\n", istr(tree_ident(d)), type_pp(tree_type(d)));
+      }
+   }
+   else if (strcmp(what, "process") == 0) {
+      for (unsigned i = 0; i < tree_stmts(top); i++) {
+         tree_t p = tree_stmt(top, i);
+         printf("%s\n", istr(tree_ident(p)));
+      }
+   }
+   else {
+      fprintf(stderr, "cannot show '%s' - try 'show help' for usage\n", what);
+      return TCL_ERROR;
+   }
+
    return TCL_OK;
 }
 
@@ -108,6 +145,7 @@ void shell_run(tree_t e)
    Tcl_CreateObjCommand(interp, "quit", shell_cmd_quit, &have_quit, NULL);
    Tcl_CreateObjCommand(interp, "run", shell_cmd_run, NULL, NULL);
    Tcl_CreateObjCommand(interp, "restart", shell_cmd_restart, NULL, NULL);
+   Tcl_CreateObjCommand(interp, "show", shell_cmd_show, e, NULL);
 
    slave_post_msg(SLAVE_RESTART, NULL, 0);
 
