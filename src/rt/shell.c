6fec8381909090961fd7cc1e047de517c4f2d666
Nick Gasson <nick@nickg.me.uk>
2012-02-14 18:39:45 +0000

    Search for tcl.h outside of tcl directory

--- src/rt/shell.c
+++ src/rt/shell.c
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2011  Nick Gasson
+//  Copyright (C) 2011-2012  Nick Gasson
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -26,7 +26,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <inttypes.h>
+
+#if defined HAVE_TCL_TCL_H
 #include <tcl/tcl.h>
+#elif defined HAVE_TCL_H
+#include <tcl.h>
+#endif
 
 #ifdef HAVE_LIBREADLINE
 #include <readline/readline.h>
