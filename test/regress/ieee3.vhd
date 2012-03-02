480e9f9b2d67f156ea36ec19ad86f7497ac1bb4e
Nick Gasson <nick@nickg.me.uk>
2012-03-02 21:33:38 +0000

    Add quick test of numeric_std package

---
+++ test/regress/ieee3.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ieee3 is
+end entity;
+
+architecture test of ieee3 is
+begin
+
+    process is
+        variable x, y, z: unsigned(7 downto 0);
+    begin
+        x := to_unsigned(3, 8);
+        y := to_unsigned(5, 8);
+        assert to_integer(x) = 3;
+        assert (x + y) = 8;
+        assert (y - x) = 2;
+        assert (x * y) = 15;
+        wait;
+    end process;
+    
+end architecture;
+    
