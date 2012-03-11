93f8cea2a22999e3fad73855d92cd43fcdfd9667
Nick Gasson <nick@nickg.me.uk>
2012-03-11 13:36:04 +0000

    Improved concat2 test

--- test/regress/concat2.vhd
+++ test/regress/concat2.vhd
@@ -1,12 +1,21 @@
 entity concat2 is
 end entity;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 architecture test of concat2 is
     type int_array is array (integer range <>) of integer;
     
     function get_array return int_array is
     begin
         return (1, 2, 3);
+    end function;
+
+    function pad(a : in int_array) return int_array is
+    begin
+        return (0, 0) & a;
     end function;
     
 begin
@@ -18,6 +27,8 @@ begin
         assert x = (1, 2, 3, 4);
         x := get_array & ( 6 => 8 );
         assert x = (1, 2, 3, 8);
+        x := pad((1, 2));
+        assert x = (0, 0, 1, 2);
         wait;
     end process;
 
