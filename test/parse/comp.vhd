7adb8cb24cc01bed30a5e330c7e3aa3c154f64c9
Nick Gasson <nick@nickg.me.uk>
2012-04-28 13:03:19 +0100

    Add missing comp.vhd test file

---
+++ test/parse/comp.vhd
@@ -0,0 +1,12 @@
+package p is
+
+    component c is
+        generic ( X : integer );
+        port ( o : out integer );
+    end component;
+
+    component foo
+        port ( x : inout integer );
+    end component foo;        
+    
+end package;
