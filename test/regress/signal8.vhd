1a6b149f08d49974ba8dba86261e5a44666c5fa6
Nick Gasson <nick@nickg.me.uk>
2012-06-01 09:07:46 +0100

    Add signal8 test

---
+++ test/regress/signal8.vhd
@@ -0,0 +1,22 @@
+entity signal8 is
+end entity;
+
+architecture test of signal8 is
+    type int_array is array (integer range <>) of integer;
+    type int_array_Nx4 is array (integer range <>) of int_array(1 to 4);
+    
+    signal a : int_array_Nx4(1 to 4);
+    signal b : int_array(1 to 4);
+begin
+
+    process is
+    begin
+        a(1)(2) <= 2;
+        --assert a(1)(2) = 2;
+--        a := ( others => ( 1, 2, 3, 4 ) );
+--        b := a(1);
+--        assert b = ( 1, 2, 3, 4);
+        wait;
+    end process;
+
+end architecture;
