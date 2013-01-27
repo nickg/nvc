1f3c0acc30a122316e14bd96f19e46a5fcda025f
Nick Gasson <nick@nickg.me.uk>
2013-01-27 14:07:18 +0000

    Make rules for VITAL packages

--- tools/fetch-ieee.sh
+++ tools/fetch-ieee.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-url="http://standards.ieee.org/downloads/1076/1076.2-1996/"
+ieee_url="http://standards.ieee.org/downloads/1076/1076.2-1996"
+vital_url="http://tams-www.informatik.uni-hamburg.de/vhdl/packages/P1076.4"
 
 cat <<EOF
 This program will download IEEE library sources from
-  $url
+  $ieee_url
+  $vital_url
 and modify them by uncommenting the definition of operator XNOR.
 EOF
 
@@ -20,21 +22,27 @@ while true; do
     esac
 done
 
-files="math_complex-body.vhdl 
-    math_complex.vhdl 
-    math_real-body.vhdl
-    math_real.vhdl 
-    numeric_bit-body.vhdl
-    numeric_bit.vhdl
-    numeric_std-body.vhdl
-    numeric_std.vhdl
-    std_logic_1164-body.vhdl
-    std_logic_1164.vhdl"
+files="$ieee_url/math_complex-body.vhdl
+    $ieee_url/math_complex.vhdl
+    $ieee_url/math_real-body.vhdl
+    $ieee_url/math_real.vhdl
+    $ieee_url/numeric_bit-body.vhdl
+    $ieee_url/numeric_bit.vhdl
+    $ieee_url/numeric_std-body.vhdl
+    $ieee_url/numeric_std.vhdl
+    $ieee_url/std_logic_1164-body.vhdl
+    $ieee_url/std_logic_1164.vhdl
+    $vital_url/timing_p.vhdl
+    $vital_url/timing_b.vhdl
+    $vital_url/prmtvs_p.vhdl
+    $vital_url/prmtvs_b.vhdl
+    $vital_url/memory_p.vhdl
+    $vital_url/memory_b.vhdl"
 
 cd "$(dirname $0)/../lib/ieee"
 
 for f in $files; do
-    wget -nv -O $f "$url/$f"
+    wget -nv -O $(basename $f) $f
 done
 
 # Uncomment operator XNOR
