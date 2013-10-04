9c6698811c3b9eddb50c6834f047a96643244534
Nick Gasson <nick@nickg.me.uk>
2013-10-04 20:35:42 +0100

    FST formatting for more data types

--- src/rt/fst.c
+++ src/rt/fst.c
@@ -21,9 +21,13 @@
 #include "common.h"
 #include "fstapi.h"
 
-static tree_t  fst_top;
-static void   *fst_ctx;
-static ident_t fst_data_i;
+static tree_t   fst_top;
+static void    *fst_ctx;
+static ident_t  fst_data_i;
+static ident_t  std_logic_i;
+static ident_t  std_ulogic_i;
+static ident_t  std_bit_i;
+static uint64_t last_time;
 
 typedef struct fst_data fst_data_t;
 
@@ -34,11 +38,8 @@ struct fst_data {
    fst_fmt_fn_t  fmt;
    range_kind_t  dir;
    const char   *map;
+   size_t        size;
 };
-
-static const char std_logic_map[] = "UX01ZWLH-";
-static const char bit_map[]       = "01";
-static uint64_t   last_time;
 
 static void fst_close(void)
 {
@@ -51,12 +52,23 @@ static void fst_fmt_int(tree_t decl, fst_data_t *data)
    uint64_t val;
    rt_signal_value(decl, &val, 1, false);
 
-   char buf[33];
-   for (int i = 0; i < 32; i++)
-      buf[31 - i] = (val & (1 << i)) ? '1' : '0';
-   buf[32] = '\0';
+   char buf[data->size + 1];
+   for (size_t i = 0; i < data->size; i++)
+      buf[data->size - 1 - i] = (val & (1 << i)) ? '1' : '0';
+   buf[data->size] = '\0';
 
-   printf("emit %s\n", buf);
+   fstWriterEmitValueChange(fst_ctx, data->handle, buf);
+}
+
+static void fst_fmt_chars(tree_t decl, fst_data_t *data)
+{
+   uint64_t vals[data->size];
+   rt_signal_value(decl, vals, data->size, false);
+
+   char buf[data->size + 1];
+   for (size_t i = 0; i < data->size; i++)
+      buf[i] = data->map[vals[i]];
+   buf[data->size] = '\0';
 
    fstWriterEmitValueChange(fst_ctx, data->handle, buf);
 }
@@ -70,6 +82,35 @@ static void fst_event_cb(uint64_t now, tree_t decl)
 
    fst_data_t *data = tree_attr_ptr(decl, fst_data_i);
    (*data->fmt)(decl, data);
+}
+
+static bool fst_can_fmt_chars(type_t type, fst_data_t *data,
+                              enum fstVarType *vt,
+                              enum fstSupplimentalDataType *sdt)
+{
+   type_t base = type_base_recur(type);
+   ident_t name = type_ident(base);
+   if (name == std_ulogic_i) {
+      if (type_ident(type) == std_logic_i)
+         *sdt = FST_SDT_VHDL_STD_LOGIC;
+      else
+         *sdt = FST_SDT_VHDL_STD_ULOGIC;
+      *vt = FST_VT_SV_LOGIC;
+      data->size = 1;
+      data->fmt  = fst_fmt_chars;
+      data->map  = "UX01ZWLH-";
+      return true;;
+   }
+   else if (name == std_bit_i) {
+      *sdt = FST_SDT_VHDL_BIT;
+      *vt  = FST_VT_SV_LOGIC;
+      data->size = 1;
+      data->fmt  = fst_fmt_chars;
+      data->map  = "01";
+      return true;
+   }
+   else
+      return false;
 }
 
 void fst_restart(void)
@@ -90,15 +131,22 @@ void fst_restart(void)
 
       enum fstVarType vt;
       enum fstSupplimentalDataType sdt;
-      int size;
       if (type_is_array(type)) {
-         type_t elem = type_base_recur(type_elem(type));
-         warn_at(tree_loc(d), "cannot represent arrays of type %s "
-                 "in LXT format", type_pp(elem));
-         free(data);
-         continue;
+         type_t elem = type_elem(type);
+         if (!fst_can_fmt_chars(elem, data, &vt, &sdt)) {
+            warn_at(tree_loc(d), "cannot represent arrays of type %s "
+                    "in FST format", type_pp(elem));
+            free(data);
+            continue;
+         }
 
-         data->dir = type_dim(type, 0).kind;
+         range_t r = type_dim(type, 0);
+
+         int64_t low, high;
+         range_bounds(r, &low, &high);
+
+         data->dir  = r.kind;
+         data->size = high - low + 1;
       }
       else {
          type_t base = type_base_recur(type);
@@ -106,12 +154,14 @@ void fst_restart(void)
          case T_INTEGER:
             sdt  = FST_SDT_VHDL_INTEGER;
             vt   = FST_VT_VCD_INTEGER;
-            size = 32;
-            data->fmt = fst_fmt_int;
+            data->size = 32;
+            data->fmt  = fst_fmt_int;
             break;
 
          case T_ENUM:
-            // TODO
+            if (fst_can_fmt_chars(type, data, &vt, &sdt))
+               break;
+            // Fall-through
 
          default:
             warn_at(tree_loc(d), "cannot represent type %s in FST format",
@@ -125,7 +175,7 @@ void fst_restart(void)
          fst_ctx,
          vt,
          FST_VD_IMPLICIT,
-         size,
+         data->size,
          istr(tree_ident(d)),
          0,
          type_pp(type),
@@ -142,7 +192,10 @@ void fst_restart(void)
 
 void fst_init(const char *file, tree_t top)
 {
-   fst_data_i = ident_new("fst_data");
+   fst_data_i   = ident_new("fst_data");
+   std_logic_i  = ident_new("IEEE.STD_LOGIC_1164.STD_LOGIC");
+   std_ulogic_i = ident_new("IEEE.STD_LOGIC_1164.STD_ULOGIC");
+   std_bit_i    = ident_new("STD.STANDARD.BIT");
 
    if ((fst_ctx = fstWriterCreate(file, 1)) == NULL)
       fatal("fstWriterCreate failed");
