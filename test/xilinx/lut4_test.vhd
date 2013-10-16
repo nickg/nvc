5a9f4890a37f21b07db7add8efd4bc8001bf3a18
Nick Gasson <nick@nickg.me.uk>
2013-10-16 21:21:56 +0100

    Quick test of Xilinx simulation libraries

--- test/xilinx/lut4_test.vhd
+++ test/xilinx/lut4_test.vhd
@@ -8,7 +8,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 architecture test of lut4_test is
-    signal i : std_logic_vector(7 downto 0);
+    signal i : std_logic_vector(3 downto 0);
     signal o : std_logic;
 begin
 
@@ -21,5 +21,19 @@ begin
             i1 => i(1),
             i2 => i(2),
             i3 => i(3) );
+
+    process is
+    begin
+        i <= "0000";
+        wait for 1 ns;
+        assert o = '1';
+        i <= "0001";
+        wait for 1 ns;
+        assert o = '0';
+        i <= "1111";
+        wait for 1 ns;
+        assert o = '1';
+        wait;
+    end process;
 
 end architecture;
