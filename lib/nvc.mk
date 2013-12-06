75abb0372858d80c3a1bc49ec66af7492166f082
Nick Gasson <nick@nickg.me.uk>
2013-12-06 19:28:36 +0000

    Restore --enable-native as default and fix build with --disable-native

--- lib/nvc.mk
+++ lib/nvc.mk
@@ -1,7 +1,12 @@
 # -*- mode: makefile-automake -*-
 
 nvc = $(nvc_verbose)$(top_builddir)/src/nvc
+
+if ENABLE_NATIVE
 codegen = $(codegen_verbose)$(top_builddir)/src/nvc
+else
+codegen = @true
+endif
 
 nvc_verbose = $(nvc_verbose_@AM_V@)
 nvc_verbose_ = $(nvc_verbose_@AM_DEFAULT_V@)
@@ -11,7 +16,7 @@ codegen_verbose = $(codegen_verbose_@AM_V@)
 codegen_verbose_ = $(codegen_verbose_@AM_DEFAULT_V@)
 
 if ENABLE_NATIVE
-codegen_verbose_0 = @echo "  NATIVE  " $@; NVC_LINK_QUIET=1 
+codegen_verbose_0 = @echo "  NATIVE  " $@; NVC_LINK_QUIET=1
 else
 codegen_verbose_0 =
 endif
