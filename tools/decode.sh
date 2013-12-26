29db671df8dee7d9226ac8508e10b5556be0027d
Nick Gasson <nick@nickg.me.uk>
2013-12-26 12:04:37 +0000

    Tweak to decode.sh

--- tools/decode.sh
+++ tools/decode.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sed 's/^.*\[\(.*\)\]$/\1/' | \
     addr2line -pife ./src/nvc | \
+    grep -v '^??' | \
     sed 's/:/ /' | \
     sed 's|/.*/||' | \
     awk '{ printf("%-25s %-10s %d\n", $1, $3, $4); }'
