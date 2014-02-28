645d2edc74ae05e55dfeffbb8bb2d26b48877db0
KIMURA Masaru <hiyuh.root@gmail.com>
2014-02-28 15:17:49 +0900

    Add NVC_FOREIGN_OBJS for issue #51.

--- src/link.c
+++ src/link.c
@@ -308,6 +308,10 @@ static void link_shared(tree_t top)
    link_output(top, "s");
 #endif
 
+   const char *objs = getenv("NVC_FOREIGN_OBJS");
+   if (objs != NULL)
+      link_arg_f("%s", objs);
+
 #ifdef IMPLIB_REQUIRED
    const char *cyglib = getenv("NVC_CYG_LIB");
    link_arg_f("-L%s", (cyglib != NULL) ? cyglib : DATADIR);
