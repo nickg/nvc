8b9a2d922366e4e95181cd6ba344c8933e86ce67
Nick Gasson <nick@nickg.me.uk>
2015-03-29 09:59:08 +0100

    Tweak to ghdl.sh

--- tools/ghdl.sh
+++ tools/ghdl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir -p ghdl
-ghdl -a --workdir=ghdl ../test/regress/$1.vhd
-ghdl --elab-run --workdir=ghdl -o ghdl/$1 $1
+mkdir -p .ghdl
+ghdl -a --workdir=.ghdl ../test/regress/$1.vhd
+ghdl --elab-run --workdir=.ghdl -o .ghdl/$1 $1
