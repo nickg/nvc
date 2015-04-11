b90ae668f51948f7e12c1fe89a72caee104e21cd
Nick Gasson <nick@nickg.me.uk>
2015-04-11 13:28:42 +0100

    Use `bit_vector` rather than `std_logic_vector` in test for issue #115

--- test/regress/issue115.vhd
+++ test/regress/issue115.vhd
@@ -1,18 +1,15 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-
 entity issue115 is
 end issue115;
 
 architecture behav of issue115 is
 
-    signal PC_OUT_bus         : STD_LOGIC_VECTOR (7 DOWNTO 0);
-    signal tmp                : STD_LOGIC_VECTOR (7 DOWNTO 0);
+    signal PC_OUT_bus         : BIT_VECTOR (7 DOWNTO 0);
+    signal tmp                : BIT_VECTOR (7 DOWNTO 0);
 
     begin
 
     process
-        procedure pc_read (signal register_data : out std_logic_vector(7 downto 0)) is
+        procedure pc_read (signal register_data : out bit_vector(7 downto 0)) is
         begin
             register_data <= PC_OUT_bus;
         end procedure pc_read;
