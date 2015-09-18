23f4f7398614c3b482ba57fae2c2dd4dd64e4f40
Michael Mitchener <mitchsm@users.noreply.github.com>
2015-09-18 10:12:39 -0400

    per LRM 1.1.1.2:  A port of any mode other than in may be unconnected or unassociated as long as its type is not an unconstrained array type.

--- src/sem.c
+++ src/sem.c
@@ -5666,7 +5666,7 @@ static bool sem_check_map(tree_t t, tree_t unit,
 
       for (int i = 0; i < nformals; i++) {
          if (!formals[i].have && !tree_has_value(formals[i].decl)
-             && (tree_subkind(formals[i].decl) != PORT_OUT)) {
+             && (tree_subkind(formals[i].decl) == PORT_IN)) {
             error_at(tree_loc(t), "missing actual for formal %s",
                      istr(tree_ident(formals[i].decl)));
             ++errors;
