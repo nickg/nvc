8c21e3b732aecacd3595d26413f6126486e3f437
Martoni <mail@fabienm.eu>
2017-06-24 12:35:22 +0200

    test/run.sh: fix little bug to launch run.sh in tools directory (#329)

--- tools/run.sh
+++ tools/run.sh
@@ -4,4 +4,4 @@ export NVC_LIBPATH=./lib/
 if [ -e lib/$1.so ]; then
     vhpi="--load lib/$1.so --vhpi-trace"
 fi
-./bin/nvc --std=$std -a ../test/regress/$1.vhd -e $* -r --trace --stats $vhpi
+../bin/nvc --std=$std -a ../test/regress/$1.vhd -e $* -r --trace --stats $vhpi
