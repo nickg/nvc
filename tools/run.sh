ea9234e74c5f6028dd83d47e1c9df559504abad7
Nick Gasson <nick@nickg.me.uk>
2017-06-24 11:39:02 +0100

    Make run.sh work from a wider variety of locations

--- tools/run.sh
+++ tools/run.sh
@@ -4,4 +4,6 @@ export NVC_LIBPATH=./lib/
 if [ -e lib/$1.so ]; then
     vhpi="--load lib/$1.so --vhpi-trace"
 fi
-../bin/nvc --std=$std -a ../test/regress/$1.vhd -e $* -r --trace --stats $vhpi
+root=$(git rev-parse --show-toplevel)
+PATH="./bin:../bin:$PATH"
+nvc --std=$std -a $root/test/regress/$1.vhd -e $* -r --trace --stats $vhpi
