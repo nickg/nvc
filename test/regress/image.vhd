8bdf777ab154c56b524add7a6776eb94619aa751
Nick Gasson <nick@nickg.me.uk>
2020-11-05 12:54:36 +0800

    Extra cases for 'IMAGE regression test

--- test/regress/image.vhd
+++ test/regress/image.vhd
@@ -1,11 +1,19 @@
 package p is
+    type enum1 is (A, B);
+
     procedure print_char(c : in character);
+    procedure print_enum1(e : in enum1);
 end package;
 
 package body p is
     procedure print_char(c : in character) is
     begin
         report character'image(c);
+    end procedure;
+
+    procedure print_enum1(e : in enum1) is
+    begin
+        report enum1'image(e);
     end procedure;
 end package body;
 
@@ -15,10 +23,13 @@ end entity;
 use work.p.all;
 
 architecture test of image is
+    type enum2 is (C, D);
 begin
 
     process is
         variable i : integer;
+        variable e1 : enum1;
+        variable e2 : enum2;
     begin
         report integer'image(4);
         report integer'image(-42);
@@ -29,6 +40,12 @@ begin
         print_char('X');
         wait for 10 ps;
         report time'image(now);
+        e1 := A;
+        e2 := C;
+        wait for 1 ns;
+        report enum1'image(e1);
+        report enum2'image(e2);
+        print_enum1(e1);
         wait;
     end process;
 
