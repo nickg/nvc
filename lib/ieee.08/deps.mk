fff2739fabb43b2710d6aec786d580f219d58580
Nick Gasson <nick@nickg.me.uk>
2021-06-12 21:21:17 +0800

    Update IEEE dependencies

--- lib/ieee.08/deps.mk
+++ lib/ieee.08/deps.mk
@@ -16,6 +16,22 @@ lib/ieee.08/_IEEE.NUMERIC_BIT.so: lib/std.08/STD.TEXTIO lib/std.08/STD.STANDARD 
 
 lib/ieee.08/_IEEE.NUMERIC_BIT.vcode: lib/std.08/STD.TEXTIO lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_bit.vhdl
 
+lib/ieee.08/IEEE.NUMERIC_BIT_UNSIGNED-body: lib/ieee.08/IEEE.NUMERIC_BIT lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_BIT_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned-body.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_BIT_UNSIGNED-body.a: lib/ieee.08/IEEE.NUMERIC_BIT lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_BIT_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned-body.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_BIT_UNSIGNED-body.so: lib/ieee.08/IEEE.NUMERIC_BIT lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_BIT_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned-body.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_BIT_UNSIGNED-body.vcode: lib/ieee.08/IEEE.NUMERIC_BIT lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_BIT_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned-body.vhdl
+
+lib/ieee.08/IEEE.NUMERIC_BIT_UNSIGNED: lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_BIT_UNSIGNED.a: lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_BIT_UNSIGNED.so: lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_BIT_UNSIGNED.vcode: lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_bit_unsigned.vhdl
+
 lib/ieee.08/IEEE.NUMERIC_STD-body: lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_STD $(top_srcdir)/lib/ieee.08/numeric_std-body.vhdl
 
 lib/ieee.08/_IEEE.NUMERIC_STD-body.a: lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_STD $(top_srcdir)/lib/ieee.08/numeric_std-body.vhdl
@@ -32,6 +48,22 @@ lib/ieee.08/_IEEE.NUMERIC_STD.so: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD
 
 lib/ieee.08/_IEEE.NUMERIC_STD.vcode: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD.TEXTIO lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_std.vhdl
 
+lib/ieee.08/IEEE.NUMERIC_STD_UNSIGNED-body: lib/ieee.08/IEEE.NUMERIC_STD lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_STD_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_std_unsigned-body.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_STD_UNSIGNED-body.a: lib/ieee.08/IEEE.NUMERIC_STD lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_STD_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_std_unsigned-body.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_STD_UNSIGNED-body.so: lib/ieee.08/IEEE.NUMERIC_STD lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_STD_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_std_unsigned-body.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_STD_UNSIGNED-body.vcode: lib/ieee.08/IEEE.NUMERIC_STD lib/std.08/STD.STANDARD lib/ieee.08/IEEE.NUMERIC_STD_UNSIGNED $(top_srcdir)/lib/ieee.08/numeric_std_unsigned-body.vhdl
+
+lib/ieee.08/IEEE.NUMERIC_STD_UNSIGNED: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_std_unsigned.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_STD_UNSIGNED.a: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_std_unsigned.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_STD_UNSIGNED.so: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_std_unsigned.vhdl
+
+lib/ieee.08/_IEEE.NUMERIC_STD_UNSIGNED.vcode: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/numeric_std_unsigned.vhdl
+
 lib/ieee.08/IEEE.STD_LOGIC_1164-body: lib/std.08/STD.STANDARD lib/ieee.08/IEEE.STD_LOGIC_1164 $(top_srcdir)/lib/ieee.08/std_logic_1164-body.vhdl
 
 lib/ieee.08/_IEEE.STD_LOGIC_1164-body.a: lib/std.08/STD.STANDARD lib/ieee.08/IEEE.STD_LOGIC_1164 $(top_srcdir)/lib/ieee.08/std_logic_1164-body.vhdl
@@ -41,6 +73,8 @@ lib/ieee.08/_IEEE.STD_LOGIC_1164-body.so: lib/std.08/STD.STANDARD lib/ieee.08/IE
 lib/ieee.08/_IEEE.STD_LOGIC_1164-body.vcode: lib/std.08/STD.STANDARD lib/ieee.08/IEEE.STD_LOGIC_1164 $(top_srcdir)/lib/ieee.08/std_logic_1164-body.vhdl
 
 lib/ieee.08/IEEE.STD_LOGIC_1164: lib/std.08/STD.TEXTIO lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/std_logic_1164.vhdl
+
+lib/ieee.08/IEEE.STD_LOGIC_TEXTIO: lib/ieee.08/IEEE.STD_LOGIC_1164 lib/std.08/STD.TEXTIO lib/std.08/STD.STANDARD $(top_srcdir)/lib/ieee.08/std_logic_textio.vhdl
 
 lib/ieee.08/IEEE.MATH_COMPLEX-body: lib/ieee.08/IEEE.MATH_REAL lib/std.08/STD.STANDARD lib/ieee.08/IEEE.MATH_COMPLEX $(top_srcdir)/lib/ieee/math_complex-body.vhdl
 
