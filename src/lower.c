972584175a728237e4104ed4202ca46d26c468ed
Nick Gasson <nick@nickg.me.uk>
2021-07-21 17:19:22 +0800

    Fix a bug with mixed array types in lower_dyn_aggregate

--- src/lower.c
+++ src/lower.c
@@ -95,6 +95,8 @@ static vcode_type_t lower_type(type_t type);
 static vcode_reg_t lower_record_eq(vcode_reg_t r0, vcode_reg_t r1, type_t type);
 static void lower_decls(tree_t scope, vcode_unit_t context);
 static vcode_reg_t lower_array_dir(type_t type, int dim, vcode_reg_t reg);
+static void lower_check_array_sizes(tree_t t, type_t ltype, type_t rtype,
+                                    vcode_reg_t lval, vcode_reg_t rval);
 
 typedef vcode_reg_t (*lower_signal_flag_fn_t)(vcode_reg_t, vcode_reg_t);
 typedef vcode_reg_t (*arith_fn_t)(vcode_reg_t, vcode_reg_t);
@@ -2218,8 +2220,17 @@ static vcode_reg_t lower_dyn_aggregate(tree_t agg, type_t type)
       if (kind != A_OTHERS) {
          tree_t value = tree_value(a);
          value_reg = lower_expr(value, EXPR_RVALUE);
-         if (type_is_scalar(tree_type(value)))
+
+         type_t value_type = tree_type(value);
+         if (type_is_scalar(value_type))
             value_reg = lower_reify(value_reg);
+         else if (type_is_array(value_type)) {
+            if (!lower_const_bounds(value_type)) {
+               lower_check_array_sizes(value, elem_type, value_type,
+                                       VCODE_INVALID_REG, value_reg);
+            }
+            value_reg = lower_array_data(value_reg);
+         }
       }
 
       if (what == VCODE_INVALID_REG) {
