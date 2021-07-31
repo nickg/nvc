64c2521260e868224ed94e6913f378c306ef2909
Nick Gasson <nick@nickg.me.uk>
2021-07-31 11:30:31 +0800

    Handle access(2) returning EPERM in macOS sandbox. Fixes #421

--- src/lib.c
+++ src/lib.c
@@ -212,7 +212,7 @@ static lib_t lib_init(const char *name, const char *rpath, int lock_fd)
       // exlusive locking on some NFS implementations
       int mode = O_RDWR;
       if (access(lock_path, mode) != 0) {
-         if (errno == EACCES) {
+         if (errno == EACCES || errno == EPERM) {
             mode = O_RDONLY;
             l->readonly = true;
          }
