776661ad71d76cd84a864b7fdb48e107913428a3
Nick Gasson <nick@nickg.me.uk>
2021-10-23 23:34:40 +0800

    Update run_vests.rb to actually run the test

--- test/run_vests.rb
+++ test/run_vests.rb
@@ -4,7 +4,8 @@ require 'rubygems'
 require 'pathname'
 require 'colorize'
 require 'timeout'
-require 'fileutils'
+require 'open3'
+require 'tmpdir'
 
 TestDir = Pathname.new(__FILE__).realpath.dirname
 BuildDir = Pathname.new(ENV['BUILD_DIR'] || Dir.pwd).realpath
@@ -16,10 +17,29 @@ Tool = 'nvc'
 Billowitch = "#{Prefix}/billowitch/compliant"
 ExpectFails = 89
 
+ENV['NVC_COLORS'] = 'always'
+
 def run_cmd(c)
-  Process.wait Process.spawn(c)
-  puts "#{c} aborted" unless $?.exitstatus < 2
-  $?.exitstatus == 0
+  Timeout.timeout(10) do
+    stdout, stderr, status = Open3.capture3(c)
+    if status != 0 then
+      puts
+      puts c.magenta
+      puts stdout
+      puts stderr
+    end
+    return status == 0
+  end
+end
+
+def guess_top(src)
+  best = nil
+  File.open(src).lines do |l|
+    if m = l.match(/^(?:entity|configuration) *(\w+)/i) then
+      best = m.captures[0]
+    end
+  end
+  return best
 end
 
 unless Dir.exists? VestsDir
@@ -37,22 +57,20 @@ passes = 0
 # Billowitch, compliant
 #
 
-Dir.chdir Billowitch
-
-Dir.foreach('.') do |item|
+Dir.foreach(Billowitch) do |item|
   next unless item =~ /\.vhdl?$/
 
-  FileUtils.rm_rf 'work'
-
-  f = File.realpath "#{Billowitch}/#{item}"
-  cmd = "#{Tool} -a #{f}"
-  if run_cmd cmd then
-    # TODO: elaborate, run
-    passes += 1
-    print '+'.green
-  else
-    puts cmd.magenta
-    fails += 1
+  Dir.mktmpdir do |tmpdir|
+    f = File.realpath "#{Billowitch}/#{item}"
+    top = guess_top f
+    #cmd = "#{Tool} --work=work:#{tmpdir} -a #{f} -e #{top} -r"
+    cmd = "#{Tool} --force-init --work=work:#{tmpdir} -a #{f}"
+    if run_cmd cmd then
+      passes += 1
+      print '+'.green
+    else
+      fails += 1
+    end
   end
 end
 
@@ -69,4 +87,3 @@ if fails > ExpectFails then
   puts "Expected #{ExpectFails} failures but have #{fails}"
   exit 1
 end
-
