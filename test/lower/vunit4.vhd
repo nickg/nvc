365818abd0f14c6ae459fa34563ad407d00fd40e
Nick Gasson <nick@nickg.me.uk>
2022-02-07 09:06:07 +0800

    Fix crash when record aggregate contains an access type

---
+++ test/lower/vunit4.vhd
@@ -0,0 +1,24 @@
+package vunit4 is
+    type rec;
+    type int_ptr is access integer;
+
+    impure function get_rec(n : natural) return rec;
+
+end package;
+
+package body vunit4 is
+
+    type rec is record
+        x : int_ptr;
+    end record;
+
+    type int_ptr_vec is array (natural range <>) of int_ptr;
+
+    shared variable v : int_ptr_vec(1 to 5);
+
+    impure function get_rec(n : natural) return rec is
+    begin
+        return rec'(x => v(n));
+    end function;
+
+end package body;
