f6fe264b38d6f3fd7f4b851a10bc4098e5541c69
Nick Gasson <nick@nickg.me.uk>
2022-08-07 21:31:19 +0100

    Import VHDL-2019 STD packages

---
+++ lib/std.19/env.vhdl
@@ -0,0 +1,307 @@
+-- -----------------------------------------------------------------
+--
+-- Copyright 2019 IEEE P1076 WG Authors
+--
+-- See the LICENSE file distributed with this work for copyright and
+-- licensing information and the AUTHORS file.
+--
+-- This file to you under the Apache License, Version 2.0 (the "License").
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+-- implied.  See the License for the specific language governing
+-- permissions and limitations under the License.
+--
+--   Title     :  Environment package
+--             :
+--   Library   :  This package shall be compiled into a library
+--             :  symbolically named std.
+--             :
+--   Developers:  IEEE P1076 Working Group
+--             :
+--   Purpose   :  This packages defines basic tool interface subprograms
+--             :
+--   Note      :  This package may be modified to include additional data
+--             :  required by tools, but it must in no way change the
+--             :  external interfaces or simulation behavior of the
+--             :  description. It is permissible to add comments and/or
+--             :  attributes to the package declarations, but not to change
+--             :  or delete any original lines of the package declaration.
+--             :  The package body may be changed only in accordance with
+--             :  the terms of Clause 16 of this standard.
+--             :
+-- --------------------------------------------------------------------
+
+use work.textio.all;
+
+
+package ENV is
+  procedure STOP (STATUS: INTEGER);
+  procedure STOP;
+  procedure FINISH (STATUS: INTEGER);
+  procedure FINISH;
+
+  function RESOLUTION_LIMIT return DELAY_LENGTH;
+
+
+  type DAYOFWEEK is (
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY,
+    FRIDAY, SATURDAY
+  );
+
+  -- Calendar date/time, broken into parts.  Second accomodates both
+  -- single and double leap-seconds.  Dayofyear accomodates leap days.
+  -- Month 0 is January, 1 is February, 11 is December. Year is absolute
+  -- in AD, 1900 represents the year 1900.
+  --
+  type TIME_RECORD is record
+    microsecond : INTEGER range 0 to 999_999;
+    second : INTEGER range 0 to 61;
+    minute : INTEGER range 0 to 59;
+    hour : INTEGER range 0 to 23;
+    day : INTEGER range 1 to 31;
+    month : INTEGER range 0 to 11;
+    year : INTEGER range 1 to 4095;
+    weekday : DAYOFWEEK;
+    dayofyear : INTEGER range 0 to 365;
+  end record TIME_RECORD;
+
+  -- Current local time broken into parts.
+  -- Minimum legal resolution is 1 second.
+  impure function LOCALTIME return TIME_RECORD;
+
+  -- Current UTC time broken into parts.
+  -- Minimum legal resolution is 1 second.
+  impure function GMTIME return TIME_RECORD;
+
+  -- Number of seconds since midnight, Jan 1 1970, UTC.
+  -- Minimum legal resolution is 1 second.
+  impure function EPOCH return REAL;
+
+  -- Time conversion functions from epoch time.
+  function LOCALTIME(TIMER: REAL) return TIME_RECORD;
+  function GMTIME(TIMER: REAL) return TIME_RECORD;
+
+  -- Time conversion function from time in parts.
+  -- EPOCH and GMTIME accept TREC in local time.
+  -- LOCALTIME accepts TREC in UTC.
+  function EPOCH(TREC: TIME_RECORD) return REAL;
+  function LOCALTIME(TREC: TIME_RECORD) return TIME_RECORD;
+  function GMTIME(TREC: TIME_RECORD) return TIME_RECORD;
+
+  -- Time increment/decrement.  DELTA argument is in seconds.
+  -- Returned TIME_RECORD is in local time or UTC per the TREC.
+  function "+"(TREC: TIME_RECORD; DELTA: REAL) return TIME_RECORD;
+  function "+"(DELTA: REAL; TREC: TIME_RECORD) return TIME_RECORD;
+  function "-"(TREC: TIME_RECORD; DELTA: REAL) return TIME_RECORD;
+  function "-"(DELTA: REAL; TREC: TIME_RECORD) return TIME_RECORD;
+
+  -- Time difference in seconds.  TR1, TR2 must both be in local
+  -- time, or both in UTC.
+  function "-"(TR1, TR2: TIME_RECORD) return REAL;
+
+  -- Conversion between real seconds and VHDL TIME.  SECONDS_TO_TIME
+  -- will cause an error if the resulting REAL_VAL would be less than
+  -- TIME'LOW or greater than TIME'HIGH.
+  function TIME_TO_SECONDS(TIME_VAL: IN TIME) return REAL;
+  function SECONDS_TO_TIME(REAL_VAL: IN REAL) return TIME;
+
+  -- Convert TIME_RECORD to a string in ISO 8601 format.
+  -- TO_STRING(x)    => "1973-09-16T01:03:52"
+  -- TO_STRING(x, 6) => "1973-09-16T01:03:52.000001"
+  function TO_STRING(TREC: TIME_RECORD;
+                     FRAC_DIGITS: INTEGER range 0 to 6 := 0)
+                     return STRING;
+
+  impure function GETENV(Name : STRING) return STRING;
+  impure function GETENV(Name : STRING) return LINE;
+
+
+  impure function VHDL_VERSION return STRING;
+  function TOOL_TYPE    return STRING;
+  function TOOL_VENDOR  return STRING;
+  function TOOL_NAME    return STRING;
+  function TOOL_EDITION return STRING;
+  function TOOL_VERSION return STRING;
+
+
+  type DIRECTORY_ITEMS is access LINE_VECTOR;
+  -- The predefined operations for this type are as follows:
+  -- function"=" (anonymous, anonymous: DIRECTORY_ITEMS) return BOOLEAN;
+  -- function"/=" (anonymous, anonymous: DIRECTORY_ITEMS) return BOOLEAN;
+  -- procedure DEALLOCATE (P: inout DIRECTORY_ITEMS);
+
+  type DIRECTORY is record
+    Name      : LINE;             -- current directory name; resolved to its canonical form
+    Items     : DIRECTORY_ITEMS;  -- list of pointers to directory item names
+  end record;
+  -- The predefined operations for this type are as follows:
+  -- function "="(anonymous, anonymous: DIRECTORY) return BOOLEAN;
+  -- function "/="(anonymous, anonymous: DIRECTORY) return BOOLEAN;
+
+  type DIR_OPEN_STATUS is (
+    STATUS_OK,
+    STATUS_NOT_FOUND,
+    STATUS_NO_DIRECTORY,
+    STATUS_ACCESS_DENIED,
+    STATUS_ERROR
+  );
+  -- The predefined operations for this type are as follows:
+  -- function "="(anonymous, anonymous: DIR_OPEN_STATUS) return BOOLEAN;
+  -- function "/="(anonymous, anonymous: DIR_OPEN_STATUS) return BOOLEAN;
+  -- function "<"(anonymous, anonymous: DIR_OPEN_STATUS) return BOOLEAN;
+  -- function "<="(anonymous, anonymous: DIR_OPEN_STATUS) return BOOLEAN;
+  -- function ">"(anonymous, anonymous: DIR_OPEN_STATUS) return BOOLEAN;
+  -- function ">="(anonymous, anonymous: DIR_OPEN_STATUS) return BOOLEAN;
+  -- function MINIMUM (L, R: DIR_OPEN_STATUS) return DIR_OPEN_STATUS;
+  -- function MAXIMUM (L, R: DIR_OPEN_STATUS) return DIR_OPEN_STATUS;
+
+  type DIR_CREATE_STATUS is (
+    STATUS_OK,
+    STATUS_ITEM_EXISTS,
+    STATUS_ACCESS_DENIED,
+    STATUS_ERROR
+  );
+  -- The predefined operations for this type are as follows:
+  -- function "="(anonymous, anonymous: DIR_CREATE_STATUS) return BOOLEAN;
+  -- function "/="(anonymous, anonymous: DIR_CREATE_STATUS) return BOOLEAN;
+  -- function "<"(anonymous, anonymous: DIR_CREATE_STATUS) return BOOLEAN;
+  -- function "<="(anonymous, anonymous: DIR_CREATE_STATUS) return BOOLEAN;
+  -- function ">"(anonymous, anonymous: DIR_CREATE_STATUS) return BOOLEAN;
+  -- function ">="(anonymous, anonymous: DIR_CREATE_STATUS) return BOOLEAN;
+  -- function MINIMUM (L, R: DIR_CREATE_STATUS) return DIR_CREATE_STATUS;
+  -- function MAXIMUM (L, R: DIR_CREATE_STATUS) return DIR_CREATE_STATUS;
+
+  type DIR_DELETE_STATUS is (
+    STATUS_OK,
+    STATUS_NO_DIRECTORY,
+    STATUS_NOT_EMPTY,
+    STATUS_ACCESS_DENIED,
+    STATUS_ERROR
+  );
+  -- The predefined operations for this type are as follows:
+  -- function "="(anonymous, anonymous: DIR_DELETE_STATUS) return BOOLEAN;
+  -- function "/="(anonymous, anonymous: DIR_DELETE_STATUS) return BOOLEAN;
+  -- function "<"(anonymous, anonymous: DIR_DELETE_STATUS) return BOOLEAN;
+  -- function "<="(anonymous, anonymous: DIR_DELETE_STATUS) return BOOLEAN;
+  -- function ">"(anonymous, anonymous: DIR_DELETE_STATUS) return BOOLEAN;
+  -- function ">="(anonymous, anonymous: DIR_DELETE_STATUS) return BOOLEAN;
+  -- function MINIMUM (L, R: DIR_DELETE_STATUS) return DIR_DELETE_STATUS;
+  -- function MAXIMUM (L, R: DIR_DELETE_STATUS) return DIR_DELETE_STATUS;
+
+  type FILE_DELETE_STATUS is (
+    STATUS_OK,
+    STATUS_NO_FILE,
+    STATUS_ACCESS_DENIED,
+    STATUS_ERROR
+  );
+  -- The predefined operations for this type are as follows:
+  -- function "="(anonymous, anonymous: FILE_DELETE_STATUS) return BOOLEAN;
+  -- function "/="(anonymous, anonymous: FILE_DELETE_STATUS) return BOOLEAN;
+  -- function "<"(anonymous, anonymous: FILE_DELETE_STATUS) return BOOLEAN;
+  -- function "<="(anonymous, anonymous: FILE_DELETE_STATUS) return BOOLEAN;
+  -- function ">"(anonymous, anonymous: FILE_DELETE_STATUS) return BOOLEAN;
+  -- function ">="(anonymous, anonymous: FILE_DELETE_STATUS) return BOOLEAN;
+  -- function MINIMUM (L, R: FILE_DELETE_STATUS) return FILE_DELETE_STATUS;
+  -- function MAXIMUM (L, R: FILE_DELETE_STATUS) return FILE_DELETE_STATUS;
+
+  procedure       DIR_OPEN(Dir : out DIRECTORY; Path : in STRING; Status : out DIR_OPEN_STATUS);
+  impure function DIR_OPEN(Dir : out DIRECTORY; Path : in STRING) return DIR_OPEN_STATUS;
+  procedure       DIR_CLOSE(Dir : in DIRECTORY);
+
+  impure function DIR_ITEMEXISTS(Path : in STRING) return BOOLEAN;
+  impure function DIR_ITEMISDIR(Path : in STRING) return BOOLEAN;
+  impure function DIR_ITEMISFILE(Path : in STRING) return BOOLEAN;
+
+  procedure       DIR_WORKINGDIR(Path : in STRING; Status : out DIR_OPEN_STATUS);
+  impure function DIR_WORKINGDIR(Path : in STRING) return DIR_OPEN_STATUS;
+  impure function DIR_WORKINGDIR return STRING;
+
+  procedure       DIR_CREATEDIR(Path : in STRING; Status : out DIR_CREATE_STATUS);
+  procedure       DIR_CREATEDIR(Path : in STRING; Parents : in BOOLEAN; Status : out DIR_CREATE_STATUS);
+  impure function DIR_CREATEDIR(Path : in STRING; Parents : in BOOLEAN := FALSE) return DIR_CREATE_STATUS;
+  procedure       DIR_DELETEDIR(Path : in STRING; Status : out DIR_DELETE_STATUS);
+  procedure       DIR_DELETEDIR(Path : in STRING; Recursive : in BOOLEAN; Status : out DIR_DELETE_STATUS);
+  impure function DIR_DELETEDIR(Path : in STRING; Recursive : in BOOLEAN := FALSE) return DIR_DELETE_STATUS;
+  procedure       DIR_DELETEFILE(Path : in STRING; Status : out FILE_DELETE_STATUS);
+  impure function DIR_DELETEFILE(Path : in STRING) return FILE_DELETE_STATUS;
+
+  constant        DIR_SEPARATOR : STRING;
+
+
+  type CALL_PATH_ELEMENT is record
+    name             : LINE;
+    file_name        : LINE;
+    file_path        : LINE;
+    file_line        : POSITIVE;
+  end record;
+  -- function "="  (anonymous, anonymous: CALL_PATH_ELEMENT) return BOOLEAN;
+  -- function "/=" (anonymous, anonymous: CALL_PATH_ELEMENT) return BOOLEAN;
+  impure function TO_STRING (call_path : CALL_PATH_ELEMENT ) return STRING;
+
+  type CALL_PATH_VECTOR is array (natural range <>) of CALL_PATH_ELEMENT;
+  -- function "="  (anonymous, anonymous: CALL_PATH_VECTOR) return BOOLEAN;
+  -- function "/=" (anonymous, anonymous: CALL_PATH_VECTOR) return BOOLEAN;
+  impure function TO_STRING (call_path : CALL_PATH_VECTOR; Separator : STRING := "" & LF ) return STRING;
+
+  type CALL_PATH_VECTOR_PTR is access CALL_PATH_VECTOR;
+  -- function "="  (anonymous, anonymous: CALL_PATH_VECTOR_PTR) return BOOLEAN;
+  -- function "/=" (anonymous, anonymous: CALL_PATH_VECTOR_PTR) return BOOLEAN;
+  -- procedure DEALLOCATE (P: inout CALL_PATH_VECTOR_PTR);
+  impure function TO_STRING (call_path : CALL_PATH_VECTOR_PTR; Separator : STRING := "" & LF ) return STRING;
+
+  impure function GET_CALL_PATH return CALL_PATH_VECTOR_PTR;
+
+  impure function FILE_NAME return LINE;
+  impure function FILE_NAME return STRING;
+  impure function FILE_PATH return LINE;
+  impure function FILE_PATH return STRING;
+  impure function FILE_LINE return POSITIVE;
+  impure function FILE_LINE return STRING;
+
+
+  -- VHDL Assert Failed
+  impure function IsVhdlAssertFailed return boolean;
+  impure function IsVhdlAssertFailed (Level : SEVERITY_LEVEL ) return boolean;
+
+  -- VHDL Assert Count
+  impure function GetVhdlAssertCount return natural;
+  impure function GetVhdlAssertCount (Level : SEVERITY_LEVEL ) return natural;
+
+  --  Clear VHDL Assert Errors
+  procedure ClearVhdlAssert;
+
+  -- Assert Enable, Disable/Ignore Asserts
+  procedure SetVhdlAssertEnable(Enable : boolean := TRUE);
+  procedure SetVhdlAssertEnable(Level : SEVERITY_LEVEL := NOTE; Enable : boolean := TRUE);
+  impure function GetVhdlAssertEnable(Level : SEVERITY_LEVEL := NOTE) return boolean;
+
+  --  Assert statement formatting
+  procedure SetVhdlAssertFormat(Level : SEVERITY_LEVEL; Format: string);
+  procedure SetVhdlAssertFormat(Level : SEVERITY_LEVEL; Format: string; Valid : out boolean);
+  impure function GetVhdlAssertFormat(Level : SEVERITY_LEVEL) return string;
+
+  --  VHDL Read Severity
+  procedure SetVhdlReadSeverity(Level: SEVERITY_LEVEL := FAILURE);
+  impure function GetVhdlReadSeverity return SEVERITY_LEVEL;
+
+  -- PSL Assert Failed
+  impure function PslAssertFailed return boolean;
+
+  -- PSL Is Covered
+  impure function PslIsCovered return boolean;
+
+  -- Psl Cover Asserts
+  procedure SetPslCoverAssert( Enable : boolean := TRUE);
+  impure function GetPslCoverAssert return boolean;
+
+  -- Psl Is AssertCovered
+  impure function PslIsAssertCovered return boolean;
+
+  -- Clear PSL State (Assert and Cover)
+  procedure ClearPslState;
+end package ENV;
---
+++ lib/std.19/reflection.vhdl
@@ -0,0 +1,302 @@
+-- -----------------------------------------------------------------
+--
+-- Copyright 2019 IEEE P1076 WG Authors
+--
+-- See the LICENSE file distributed with this work for copyright and
+-- licensing information and the AUTHORS file.
+--
+-- This file to you under the Apache License, Version 2.0 (the "License").
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+-- implied.  See the License for the specific language governing
+-- permissions and limitations under the License.
+--
+
+package REFLECTION is
+  type    INDEX           is       range INTEGER'low to INTEGER'high;
+  subtype NATURAL_INDEX   is INDEX range 0 to INDEX'high;
+  subtype POSITIVE_INDEX  is INDEX range 1 to INDEX'high;
+  subtype DIMENSION       is INDEX range 1 to INDEX'high;
+  type    INDEX_VECTOR    is array(DIMENSION range <>) of INDEX;
+
+  -- Incomplete type declarations
+  type VALUE_MIRROR;
+  type SUBTYPE_MIRROR;
+
+  -- Enumeration subtype/value mirror
+  type ENUMERATION_SUBTYPE_MIRROR;
+  type ENUMERATION_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return ENUMERATION_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+    impure function pos   return INTEGER;
+    impure function image return STRING;
+  end protected;
+  type ENUMERATION_VALUE_MIRROR is access ENUMERATION_VALUE_MIRROR_PT;
+
+  type ENUMERATION_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function enumeration_literal(literal_idx : NATURAL_INDEX) return ENUMERATION_VALUE_MIRROR;
+    impure function enumeration_literal(literal_name : STRING)       return ENUMERATION_VALUE_MIRROR;
+
+    impure function simple_name return STRING;
+    impure function left        return ENUMERATION_VALUE_MIRROR;
+    impure function right       return ENUMERATION_VALUE_MIRROR;
+    impure function low         return ENUMERATION_VALUE_MIRROR;
+    impure function high        return ENUMERATION_VALUE_MIRROR;
+    impure function length      return POSITIVE_INDEX;
+    impure function ascending   return BOOLEAN;
+  end protected;
+  type ENUMERATION_SUBTYPE_MIRROR is access ENUMERATION_SUBTYPE_MIRROR_PT;
+
+
+  -- Integer subtype/value mirror
+  type INTEGER_SUBTYPE_MIRROR;
+  type INTEGER_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return INTEGER_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    impure function value return INTEGER;
+    impure function image return STRING;
+  end protected;
+  type INTEGER_VALUE_MIRROR is access INTEGER_VALUE_MIRROR_PT;
+
+  type INTEGER_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function simple_name return STRING;
+    impure function left        return INTEGER_VALUE_MIRROR;
+    impure function right       return INTEGER_VALUE_MIRROR;
+    impure function low         return INTEGER_VALUE_MIRROR;
+    impure function high        return INTEGER_VALUE_MIRROR;
+    impure function length      return INDEX;
+    impure function ascending   return BOOLEAN;
+  end protected;
+  type INTEGER_SUBTYPE_MIRROR is access INTEGER_SUBTYPE_MIRROR_PT;
+
+
+  -- Floating-point subtype/value mirror
+  type FLOATING_SUBTYPE_MIRROR;
+  type FLOATING_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return FLOATING_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    impure function value return REAL;
+    impure function image return STRING;
+  end protected;
+  type FLOATING_VALUE_MIRROR is access FLOATING_VALUE_MIRROR_PT;
+
+  type FLOATING_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function simple_name return STRING;
+    impure function left        return FLOATING_VALUE_MIRROR;
+    impure function right       return FLOATING_VALUE_MIRROR;
+    impure function low         return FLOATING_VALUE_MIRROR;
+    impure function high        return FLOATING_VALUE_MIRROR;
+    impure function ascending   return BOOLEAN;
+  end protected;
+  type FLOATING_SUBTYPE_MIRROR is access FLOATING_SUBTYPE_MIRROR_PT;
+
+
+  -- Physical subtype/value mirror
+  type PHYSICAL_SUBTYPE_MIRROR;
+  type PHYSICAL_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return PHYSICAL_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    impure function unit_index return INDEX;
+    impure function value      return INTEGER;
+    impure function image      return STRING;
+  end protected;
+  type PHYSICAL_VALUE_MIRROR is access PHYSICAL_VALUE_MIRROR_PT;
+
+  type PHYSICAL_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function units_length                   return INDEX;
+    impure function unit_name (unit_idx: INDEX)    return STRING;
+    impure function unit_index(unit_name : STRING) return INDEX;
+    impure function scale(unit_idx: INDEX)         return NATURAL;
+    impure function scale(unit_name: STRING)       return NATURAL;
+
+    impure function simple_name return STRING;
+    impure function left        return PHYSICAL_VALUE_MIRROR;
+    impure function right       return PHYSICAL_VALUE_MIRROR;
+    impure function low         return PHYSICAL_VALUE_MIRROR;
+    impure function high        return PHYSICAL_VALUE_MIRROR;
+    impure function length      return INDEX;
+    impure function ascending   return BOOLEAN;
+  end protected;
+  type PHYSICAL_SUBTYPE_MIRROR is access PHYSICAL_SUBTYPE_MIRROR_PT;
+
+
+  -- Record subtype/value mirror
+  type RECORD_SUBTYPE_MIRROR;
+  type RECORD_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return RECORD_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    impure function get(element_idx : INDEX)   return VALUE_MIRROR;
+    impure function get(element_name : STRING) return VALUE_MIRROR;
+  end protected;
+  type RECORD_VALUE_MIRROR is access RECORD_VALUE_MIRROR_PT;
+
+  type RECORD_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function length                                 return INDEX;
+    impure function element_name(element_idx : INDEX)      return STRING;
+    impure function element_index(element_name : STRING)   return INDEX;
+    impure function element_subtype(element_idx : INDEX)   return SUBTYPE_MIRROR;
+    impure function element_subtype(element_name : STRING) return SUBTYPE_MIRROR;
+
+    impure function simple_name return STRING;
+  end protected;
+  type RECORD_SUBTYPE_MIRROR is access RECORD_SUBTYPE_MIRROR_PT;
+
+
+  -- Array subtype/value mirror
+  type ARRAY_SUBTYPE_MIRROR;
+  type ARRAY_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return ARRAY_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    impure function get(idx : INDEX)              return VALUE_MIRROR;
+    impure function get(idx1, idx2 : INDEX)       return VALUE_MIRROR;
+    impure function get(idx1, idx2, idx3 : INDEX) return VALUE_MIRROR;
+    impure function get(idx : INDEX_VECTOR)       return VALUE_MIRROR;
+  end protected;
+  type ARRAY_VALUE_MIRROR is access ARRAY_VALUE_MIRROR_PT;
+
+  type ARRAY_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function dimensions                          return DIMENSION;
+    impure function index_subtype(idx : DIMENSION := 1) return SUBTYPE_MIRROR;
+    impure function element_subtype                     return SUBTYPE_MIRROR;
+
+    impure function simple_name                     return STRING;
+    impure function left(idx : DIMENSION := 1)      return INDEX;
+    impure function right(idx : DIMENSION := 1)     return INDEX;
+    impure function low(idx : DIMENSION := 1)       return INDEX;
+    impure function high(idx : DIMENSION := 1)      return INDEX;
+    impure function length(idx : DIMENSION := 1)    return INDEX;
+    impure function ascending(idx : DIMENSION := 1) return BOOLEAN;
+  end protected;
+  type ARRAY_SUBTYPE_MIRROR is access ARRAY_SUBTYPE_MIRROR_PT;
+
+
+  -- Access subtype/value mirror
+  type ACCESS_SUBTYPE_MIRROR;
+  type ACCESS_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return ACCESS_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    --
+    impure function get return VALUE_MIRROR;
+    impure function is_null return BOOLEAN;
+  end protected;
+  type ACCESS_VALUE_MIRROR is access ACCESS_VALUE_MIRROR_PT;
+
+  type ACCESS_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function simple_name        return STRING;
+    impure function designated_subtype return SUBTYPE_MIRROR;
+  end protected;
+  type ACCESS_SUBTYPE_MIRROR is access ACCESS_SUBTYPE_MIRROR_PT;
+
+
+  -- File subtype/value mirror
+  type FILE_SUBTYPE_MIRROR;
+  type FILE_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return FILE_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+
+    impure function get_file_logical_name return STRING;
+    impure function get_file_open_kind    return FILE_OPEN_KIND;
+  end protected;
+  type FILE_VALUE_MIRROR is access FILE_VALUE_MIRROR_PT;
+
+  type FILE_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function simple_name        return STRING;
+    impure function designated_subtype return SUBTYPE_MIRROR;
+  end protected;
+  type FILE_SUBTYPE_MIRROR is access FILE_SUBTYPE_MIRROR_PT;
+
+
+  -- Protected subtype/value mirror
+  type PROTECTED_SUBTYPE_MIRROR;
+  type PROTECTED_VALUE_MIRROR_PT is protected
+    impure function get_subtype_mirror return PROTECTED_SUBTYPE_MIRROR;
+    impure function to_value_mirror    return VALUE_MIRROR;
+  end protected;
+  type PROTECTED_VALUE_MIRROR is access PROTECTED_VALUE_MIRROR_PT;
+
+  type PROTECTED_SUBTYPE_MIRROR_PT is protected
+    impure function to_subtype_mirror return SUBTYPE_MIRROR;
+
+    impure function simple_name return STRING;
+  end protected;
+  type PROTECTED_SUBTYPE_MIRROR is access PROTECTED_SUBTYPE_MIRROR_PT;
+
+
+  -- Type classes and sub-classes
+  type TYPE_CLASS is (
+    CLASS_ENUMERATION,
+    CLASS_INTEGER,
+    CLASS_FLOATING,
+    CLASS_PHYSICAL,
+    CLASS_RECORD,
+    CLASS_ARRAY,
+    CLASS_ACCESS,
+    CLASS_FILE,
+    CLASS_PROTECTED
+  );
+  alias VALUE_CLASS is TYPE_CLASS;
+
+
+  -- Subtype/value mirror
+  type SUBTYPE_MIRROR_PT is protected
+    impure function get_type_class return TYPE_CLASS;
+
+    -- Get the corresponding representation
+    impure function to_enumeration return ENUMERATION_SUBTYPE_MIRROR;
+    impure function to_integer     return INTEGER_SUBTYPE_MIRROR;
+    impure function to_floating    return FLOATING_SUBTYPE_MIRROR;
+    impure function to_physical    return PHYSICAL_SUBTYPE_MIRROR;
+    impure function to_record      return RECORD_SUBTYPE_MIRROR;
+    impure function to_array       return ARRAY_SUBTYPE_MIRROR;
+    impure function to_access      return ACCESS_SUBTYPE_MIRROR;
+    impure function to_file        return FILE_SUBTYPE_MIRROR;
+    impure function to_protected   return PROTECTED_SUBTYPE_MIRROR;
+
+    impure function simple_name return STRING;
+  end protected;
+  type SUBTYPE_MIRROR is access SUBTYPE_MIRROR_PT;
+
+  type VALUE_MIRROR_PT is protected
+    impure function get_value_class    return VALUE_CLASS;
+    impure function get_subtype_mirror return SUBTYPE_MIRROR;
+
+    -- Get the corresponding representation
+    impure function to_enumeration return ENUMERATION_VALUE_MIRROR;
+    impure function to_integer     return INTEGER_VALUE_MIRROR;
+    impure function to_floating    return FLOATING_VALUE_MIRROR;
+    impure function to_physical    return PHYSICAL_VALUE_MIRROR;
+    impure function to_record      return RECORD_VALUE_MIRROR;
+    impure function to_array       return ARRAY_VALUE_MIRROR;
+    impure function to_access      return ACCESS_VALUE_MIRROR;
+    impure function to_file        return FILE_VALUE_MIRROR;
+    impure function to_protected   return PROTECTED_VALUE_MIRROR;
+  end protected;
+  type VALUE_MIRROR is access VALUE_MIRROR_PT;
+end package REFLECTION;
