2653d441a8db977f90341f241b1218a45ae2ef5f
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2022-08-12 12:57:33 +0200

    src/names: Reset statement counter in T_CONCURRENT. Start counting statements from 0 in processes corresponding to each concurrent statement.

--- src/names.c
+++ src/names.c
@@ -1178,8 +1178,9 @@ ident_t get_implicit_label(tree_t t, nametab_t *tab)
    char buf[22];
 
    switch (tree_kind(t)) {
-   case T_PROCESS:
    case T_CONCURRENT:
+      tab->top_scope->lbl_cnts.stmt = 0;
+   case T_PROCESS:   
       cnt = &(tab->top_scope->lbl_cnts.proc);
       c = 'P';
       break;
