4fb1e6fbce411a16958ea4c790a6c3ceb1c2eabe
Nick Gasson <nick@nickg.me.uk>
2022-08-25 09:05:56 +0100

    Update Quartus install script
    Co-authored-by: Adam Barnes <ambarnes@gmail.com>

--- contrib/install-quartus.sh
+++ contrib/install-quartus.sh
@@ -6,11 +6,7 @@
 . $(dirname $BASH_SOURCE)/functions.sh
 
 if [ -n "$MSYSTEM" ]; then
-  # MSYS2/Windows hack
-  # Only do this if QUARTUS_ROOTDIR exists
-  # Don't do this on Linux
   QUARTUS_ROOTDIR=$(cygpath -u $QUARTUS_ROOTDIR)
-  echo QUARTUS_ROOTDIR=$QUARTUS_ROOTDIR
 fi
 
 if [ -z "$QUARTUS_ROOTDIR" ]; then
@@ -38,7 +34,7 @@ echo
 
 src=$QUARTUS_ROOTDIR/eda/sim_lib
 
-GLOBAL_OPTS="-M 32m"
+GLOBAL_OPTS="-M 128m"
 A_OPTS="--relaxed"
 
 for STD in 1993 2008; do
@@ -72,59 +68,6 @@ $src/sgate_pack.vhd
 $src/sgate.vhd
 EOF
 
-  analyse_list arriaii$(std_suffix $STD) <<EOF
-$src/arriaii_atoms.vhd
-$src/arriaii_components.vhd
-$src/arriaii_hssi_atoms.vhd
-$src/arriaii_hssi_components.vhd
-$src/arriaii_pcie_hip_atoms.vhd
-$src/arriaii_pcie_hip_components.vhd
-EOF
-
-  analyse_list arriav$(std_suffix $STD) <<EOF
-$src/arriav_atoms.vhd
-$src/arriav_components.vhd
-$src/arriav_hssi_components.vhd
-$src/arriav_hssi_atoms.vhd
-EOF
-
-  analyse_list arriavgz$(std_suffix $STD) <<EOF
-$src/arriavgz_atoms.vhd
-$src/arriavgz_components.vhd
-$src/arriavgz_hssi_components.vhd
-$src/arriavgz_hssi_atoms.vhd
-$src/arriavgz_pcie_hip_atoms.vhd
-$src/arriavgz_pcie_hip_components.vhd
-EOF
-
-  analyse_list cycloneiii$(std_suffix $STD) <<EOF
-$src/cycloneiii_atoms.vhd
-$src/cycloneiii_components.vhd
-EOF
-
-  analyse_list cycloneiv$(std_suffix $STD) <<EOF
-$src/cycloneiv_atoms.vhd
-$src/cycloneiv_components.vhd
-$src/cycloneiv_hssi_atoms.vhd
-$src/cycloneiv_hssi_components.vhd
-$src/cycloneiv_pcie_hip_atoms.vhd
-$src/cycloneiv_pcie_hip_components.vhd
-$src/cycloneive_atoms.vhd
-$src/cycloneive_components.vhd
-EOF
-
-  analyse_list cyclonev$(std_suffix $STD) <<EOF
-$src/cyclonev_atoms.vhd
-$src/cyclonev_components.vhd
-$src/cyclonev_hssi_atoms.vhd
-$src/cyclonev_hssi_components.vhd
-EOF
-
-  analyse_list cyclone10lp$(std_suffix $STD) <<EOF
-cyclone10lp_atoms.vhd
-cyclone10lp_components.vhd
-EOF
-
   analyse_list max$(std_suffix $STD) <<EOF
 $src/max_atoms.vhd
 $src/max_components.vhd
@@ -141,8 +84,48 @@ $src/maxv_components.vhd
 EOF
 
   analyse_list fiftyfivenm$(std_suffix $STD) <<EOF
-fiftyfivenm_atoms.vhd
-fiftyfivenm_components.vhd
+$src/fiftyfivenm_atoms.vhd
+$src/fiftyfivenm_components.vhd
 EOF
+
+  analyse_list cycloneiv$(std_suffix $STD) <<EOF
+$src/cycloneiv_atoms.vhd
+$src/cycloneiv_components.vhd
+$src/cycloneiv_hssi_components.vhd
+$src/cycloneiv_hssi_atoms.vhd
+$src/cycloneive_atoms.vhd
+$src/cycloneive_components.vhd
+EOF
+
+# produces compilation error
+#  analyse_list cycloneiv_pcie_hip$(std_suffix $STD) <<EOF
+#$src/cycloneiv_pcie_hip_components.vhd
+#$src/cycloneiv_pcie_hip_atoms.vhd
+#EOF
+
+  analyse_list cyclonev$(std_suffix $STD) <<EOF
+$src/cyclonev_atoms.vhd
+$src/cyclonev_components.vhd
+$src/cyclonev_hssi_components.vhd
+$src/cyclonev_hssi_atoms.vhd
+EOF
+
+  analyse_list cyclone10lp$(std_suffix $STD) <<EOF
+$src/cyclone10lp_atoms.vhd
+$src/cyclone10lp_components.vhd
+EOF
+
+  analyse_list arriaii$(std_suffix $STD) <<EOF
+$src/arriaii_atoms.vhd
+$src/arriaii_components.vhd
+$src/arriaii_hssi_components.vhd
+$src/arriaii_hssi_atoms.vhd
+EOF
+
+# produces compilation error
+#  analyse_list arriaii_pcie_hip$(std_suffix $STD) <<EOF
+#$src/arriaii_pcie_hip_components.vhd
+#$src/arriaii_pcie_hip_atoms.vhd
+#EOF
 
 done
