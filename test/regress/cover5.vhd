268765ab5dd938e71d63712f11ae3d110b212666
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2022-11-20 17:16:49 +0100

    test: Extend coverage merging test. Check outputs of each run.

--- test/regress/cover5.vhd
+++ test/regress/cover5.vhd
@@ -11,7 +11,7 @@ end entity;
 
 architecture test of cover5 is
 
-    signal vect : std_logic_vector(4 downto 0) := "10101";
+    signal vect : std_logic_vector(15 downto 0) := x"AAAA";
 
     -- Needed to pass via signal to avoid optimizing out parts of
     -- the if/else conditions.
@@ -22,7 +22,7 @@ begin
     -- Check on statements and branches
     process(cnt)
     begin
-        l_if1: if (cnt = 1) then report "IF1: cnt = 0";
+        l_if1: if (cnt = 1) then report "IF1: cnt = 1";
         elsif (cnt = 2) then     report "IF1: cnt = 2";
         elsif (cnt = 3) then     report "IF1: cnt = 3";
         else                     report "IF1: cnt = others";
@@ -33,9 +33,32 @@ begin
     process
     begin
         wait for 1 ns;
-        vect(G_VAL) <= not vect(G_VAL);
+        for i in 1 to G_VAL loop
+            vect(i * G_VAL - 1) <= not vect(i * G_VAL - 1);
+        end loop;
         wait for 1 ns;
         wait;
+    end process;
+
+    -- Check with different amount of statements in each branch
+    -- to get different coverage results for each run!
+    process(cnt)
+    begin
+        l_if2: if (cnt = 1) then
+            report "IF2: cnt = 1/1";
+        elsif (cnt = 2) then
+            report "IF2: cnt = 1/2";
+            report "IF2: cnt = 2/2";
+        elsif (cnt = 3) then
+            report "IF2: cnt = 1/3";
+            report "IF2: cnt = 2/3";
+            report "IF2: cnt = 3/3";
+        elsif (cnt = 4) then
+            report "IF2: cnt = 1/4";
+            report "IF2: cnt = 2/4";
+            report "IF2: cnt = 3/4";
+            report "IF2: cnt = 4/4";
+        end if;
     end process;
 
 end architecture;
