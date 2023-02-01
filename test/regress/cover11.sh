6007bd52b4102ae87fd9729665ff34f7e10f5384
ondrej.ille <ondrej.ille@tropicsquare.com>
2023-02-01 08:43:40 +0100

    test: Remove log file from cover11 to avoid large log file. Rely no grep of report only.

--- test/regress/cover11.sh
+++ test/regress/cover11.sh
@@ -6,7 +6,7 @@ which nvc
 nvc -a $TESTDIR/regress/cover11.vhd -e --cover=all cover11 -r
 nvc -c --item-limit=2000 \
        --exclude-file=$TESTDIR/regress/data/cover11_ef1.txt \
-       --report html work/_WORK.COVER11.elab.covdb 2>&1 | tee out.txt
+       --report html work/_WORK.COVER11.elab.covdb
 
 # Check just items until 2000 are printed
 if grep -e "EXAMPLE_ARRAY(2001)" html/hier/*; then
@@ -16,6 +16,3 @@ if ! grep -e "EXAMPLE_ARRAY(2000)" html/hier/*; then
   exit 1
 fi
 
-sed -i -e "s/[^ ]*regress\/data\//data\//g" out.txt
-
-#diff -u $TESTDIR/regress/gold/cover11.txt out.txt
