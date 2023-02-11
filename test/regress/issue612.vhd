6e2b07ce5ec7050c3871a980ee5499c1edc1118d
Nick Gasson <nick@nickg.me.uk>
2023-02-11 00:11:46 +0000

    Improve checking for issue #612 test case

--- test/regress/issue612.vhd
+++ test/regress/issue612.vhd
@@ -24,6 +24,30 @@ begin
         many_args(v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10,
                   v11, v12, v13, v14, v15, v16, v17, v18, v19,
                   v20, v21, v22, v23);
+        assert v0 = 0;
+        assert v1 = 1;
+        assert v2 = 2;
+        assert v3 = 3;
+        assert v4 = 4;
+        assert v5 = 5;
+        assert v6 = 6;
+        assert v7 = 7;
+        assert v8 = 8;
+        assert v9 = 9;
+        assert v10 = 10;
+        assert v11 = 11;
+        assert v12 = 12;
+        assert v13 = 13;
+        assert v14 = 14;
+        assert v15 = 15;
+        assert v16 = 16;
+        assert v17 = 17;
+        assert v18 = 18;
+        assert v19 = 19;
+        assert v20 = 20;
+        assert v21 = 21;
+        assert v22 = 22;
+        assert v23 = 23;
         wait;
     end process;
 
