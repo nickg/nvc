04244c435c99f58bca2ecc7158380c934f16e1cf
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2023-04-19 23:47:17 +0200

    test: Add parse4 PSL test.

---
+++ test/psl/parse4.vhd
@@ -0,0 +1,57 @@
+Library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity parse3 is
+end entity;
+
+architecture test of parse3 is
+
+    signal a, b, c, clk : bit;
+
+begin
+
+    -- psl default clock is rising_edge(clk);
+
+    -- psl sequence seq_a is {a;b;c};
+    -- psl sequence seq_b(const rpt) is a[*rpt];
+
+    -- Sequence_Instance
+    -- psl cover seq_a;
+
+    -- Sequence_Instance(Actual_Parameter_List)
+    -- psl cover seq_b(5);
+    -- psl cover seq_b(4, {a;b;c});
+
+    -- Boolean [* [ Count ] ]
+    -- psl cover a[*10];
+    -- psl cover a='1'[*1][*2][*3];
+
+    -- Sequence [* [ Count ] ]
+    -- psl cover seq_b(4)[*2];
+    -- psl cover seq_b(4)[*2][*3];
+
+    -- [* [ Count ] ]
+    -- psl cover [*5];
+    -- psl cover [*5 to 10];
+
+    -- [+]
+    -- psl cover [+];
+
+    -- Boolean [= Count ]
+    -- psl cover c[=10];
+    -- psl cover c[=5 to 6];
+    -- psl cover c[=10][*3];
+
+    -- Boolean [-> [ positive_Count ] ]
+    -- psl cover b[->4];
+
+    -- Boolean Proc_Block
+    -- psl cover a[[signal sig : std_logic; sig <= '1';]];
+
+    -- Sequence Proc_Block
+    -- psl cover {a;b} [[signal sig : unsigned(3 downto 0); sig <= "0000";]];
+
+    -- psl cover {a;b;c} @ rising_edge(clk);
+
+end architecture;
--- test/test_psl.c
+++ test/test_psl.c
@@ -137,6 +137,82 @@ START_TEST(test_parse3)
 }
 END_TEST
 
+START_TEST(test_parse4)
+{
+   opt_set_int(OPT_PSL_COMMENTS, 1);
+
+   input_from_file(TESTDIR "/psl/parse4.vhd");
+
+   tree_t a = parse_and_check(T_ENTITY, T_ARCH);
+
+   psl_node_t p0 = tree_psl(tree_stmt(a, 1));
+   fail_unless(psl_kind(p0) == P_SEQUENCE_DECL);
+
+   psl_node_t p1 = tree_psl(tree_stmt(a, 2));
+   fail_unless(psl_kind(p1) == P_SEQUENCE_DECL);
+
+   psl_node_t p3 = tree_psl(tree_stmt(a, 3));
+   psl_node_t p3_v = psl_value(p3);
+   psl_node_t p3_v_op = psl_operand(p3_v, 0);
+   fail_unless(psl_kind(p3) == P_COVER);
+   fail_unless(psl_kind(p3_v) == P_SERE);
+   fail_unless(psl_kind(p3_v_op) == P_SEQUENCE_INST);
+
+
+   psl_node_t p6 = tree_psl(tree_stmt(a, 6));
+   psl_node_t p6_v = psl_value(p6);
+   psl_node_t p6_v_r = psl_repeat(p6_v);
+   fail_unless(psl_kind(p6_v_r) == P_REPEAT);
+   fail_unless(psl_subkind(p6_v_r) == PSL_TIMES_REPEAT);
+
+   psl_node_t p7 = tree_psl(tree_stmt(a, 7));
+   psl_node_t p7_v = psl_value(p7);
+   psl_node_t p7_v_r_1 = psl_repeat(p7_v);
+
+   fail_unless(psl_kind(p7_v) == P_SERE);
+   fail_unless(tree_ival(tree_left(psl_tree(p7_v_r_1))) == 3);
+
+   psl_node_t p7_v_2 = psl_operand(p7_v, 0);
+   psl_node_t p7_v_r_2 = psl_repeat(p7_v_2);
+
+   fail_unless(psl_kind(p7_v_2) == P_SERE);
+   fail_unless(tree_ival(tree_left(psl_tree(p7_v_r_2))) == 2);
+
+   psl_node_t p7_v_3 = psl_operand(p7_v_2, 0);
+   psl_node_t p7_v_r_3 = psl_repeat(p7_v_3);
+
+   fail_unless(psl_kind(p7_v_3) == P_SERE);
+   fail_unless(tree_ival(tree_left(psl_tree(p7_v_r_3))) == 1);
+
+
+   psl_node_t p11 = tree_psl(tree_stmt(a, 11));
+   psl_node_t p11_v = psl_value(p11);
+   psl_node_t p11_v_r = psl_repeat(p11_v);
+
+   fail_unless(psl_kind(p11_v_r) == P_REPEAT);
+   fail_unless(tree_ival(tree_left(psl_tree(p11_v_r))) == 5);
+   fail_unless(tree_ival(tree_right(psl_tree(p11_v_r))) == 10);
+
+
+   psl_node_t p12 = tree_psl(tree_stmt(a, 12));
+   psl_node_t p12_v = psl_value(p12);
+   psl_node_t p12_v_r = psl_repeat(p12_v);
+
+   fail_unless(psl_kind(p12_v_r) == P_REPEAT);
+   fail_unless(psl_subkind(p12_v_r) == PSL_PLUS_REPEAT);
+
+
+   psl_node_t p17 = tree_psl(tree_stmt(a, 17));
+   psl_node_t p17_v = psl_value(p17);
+   fail_unless(psl_decls(p17_v) == 1);
+   fail_unless(tree_kind(psl_decl(p17_v, 0)) == T_BLOCK);
+
+
+
+   fail_if_errors();
+}
+END_TEST
+
 START_TEST(test_dump)
 {
    opt_set_int(OPT_PSL_COMMENTS, 1);
@@ -173,6 +249,7 @@ Suite *get_psl_tests(void)
    tcase_add_test(tc_core, test_parse2);
    tcase_add_test(tc_core, test_sem1);
    tcase_add_test(tc_core, test_parse3);
+   tcase_add_test(tc_core, test_parse4);
    tcase_add_test(tc_core, test_dump);
    suite_add_tcase(s, tc_core);
 
