08de06bb901d7ee3c373cae1aab06eb0de845323
Nick Gasson <nick@nickg.me.uk>
2023-04-22 14:11:37 +0100

    Bump OSVVM to 2023.04

--- contrib/install-osvvm.tcl
+++ contrib/install-osvvm.tcl
@@ -1,4 +1,9 @@
 source "Scripts/StartNVC.tcl"
 set ::osvvm::VhdlLibraryDirectory "${::env(NVC_INSTALL_DEST)}"
 set ::osvvm::VhdlLibrarySubdirectory "."
-source "OsvvmLibraries.pro"
+
+foreach std {2008} {
+    SetVHDLVersion $std
+    source "OsvvmLibraries.pro"
+}
+
--- test/test-osvvm.tcl
+++ test/test-osvvm.tcl
@@ -4,10 +4,10 @@
 # Note: tcllib is required for OSVVM
 
 # known good snapshot
-set OsvvmLibraries_tag "2022.11"
+set OsvvmLibraries_tag "2023.04"
+set OsvvmDir [file join $::env(HOME) .cache nvc "OsvvmLibraries-${OsvvmLibraries_tag}"]
 
-exec git clone --branch $OsvvmLibraries_tag --recurse-submodules -j8 https://github.com/OSVVM/OsvvmLibraries.git >@ stdout 2>@ stdout
-source OsvvmLibraries/Scripts/StartNVC.tcl
-build OsvvmLibraries/OsvvmLibraries
-build OsvvmLibraries/RunAllTests
-build OsvvmLibraries/RunAllTestsVti
+source $OsvvmDir/Scripts/StartNVC.tcl
+#build $OsvvmDir/OsvvmLibraries
+build $OsvvmDir/RunAllTests
+build $OsvvmDir/RunAllTestsVti
