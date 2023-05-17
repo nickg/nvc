nickg/nvc

commit e9ec0c7026e1e7774c0bb0e3e68b001006a57ee8

vhpi: Support enabling, disabling, and removing callbacks
Add support for enabling and disabling callbacks separate from
registering a new callback. Also support removing callbacks (and freeing
the handle).

src/rt/model.c
@@ -3195,7 +3195,7 @@ void model_clear_global_cb(rt_model_t *m, rt_event_t event, rt_event_fn_t fn,
    rt_callback_t **last = &m->global_cbs[event];
    for (rt_callback_t *it = *last; it; last = &(it->next), it = it->next) {
       if (it->fn == fn && it->user == user) {
-         last = &(it->next);
+         *last = it->next;
          free(it);
          return;
       }

src/vhpi/vhpi-model.c
@@ -232,6 +232,7 @@ typedef struct {
    vhpiStateT   State;
    vhpiEnumT    Reason;
    vhpiCbDataT  data;
+   uint64_t     when;
 } c_callback;
 
 DEF_CLASS(callback, vhpiCallbackK, object);
@@ -601,7 +602,17 @@ int vhpi_release_handle(vhpiHandleT handle)
 
    VHPI_TRACE("handle=%s", handle_pp(handle));
 
-   return 0;
+   c_vhpiObject *obj = from_handle(handle);
+   if (obj == NULL)
+      return 1;
+
+   switch (obj->kind) {
+   case vhpiCallbackK:
+      free(obj);
+      return 0;
+   default:
+      return 0;
+   }
 }
 
 static int enable_cb(c_callback *cb)
@@ -626,8 +637,8 @@ static int enable_cb(c_callback *cb)
             return 1;
          }
 
-         uint64_t when = vhpi_time_to_native(cb->data.time) + model_now(model, NULL);
-         model_set_timeout_cb(model, when, vhpi_timeout_cb, cb);
+         cb->when = vhpi_time_to_native(cb->data.time) + model_now(model, NULL);
+         model_set_timeout_cb(model, cb->when, vhpi_timeout_cb, cb);
          return 0;
       }
 
@@ -674,6 +685,34 @@ vhpiHandleT vhpi_register_cb(vhpiCbDataT *cb_data_p, int32_t flags)
    return (flags & vhpiReturnCb) ? handle_for(&(cb->object)) : NULL;
 }
 
+static int disable_cb(c_callback *cb)
+{
+   switch (cb->Reason) {
+   case vhpiCbRepEndOfProcesses:
+   case vhpiCbRepLastKnownDeltaCycle:
+   case vhpiCbRepNextTimeStep:
+   case vhpiCbEndOfProcesses:
+   case vhpiCbStartOfSimulation:
+   case vhpiCbEndOfSimulation:
+   case vhpiCbLastKnownDeltaCycle:
+   case vhpiCbNextTimeStep:
+      model_clear_global_cb(model, vhpi_get_rt_event(cb->Reason),
+                            vhpi_global_cb, cb);
+      return 0;
+
+   case vhpiCbAfterDelay:
+      model_clear_timeout_cb(model, cb->when, vhpi_timeout_cb, cb);
+      return 0;
+
+   case vhpiCbValueChange:
+      return 1;
+
+   default:
+      assert(false);
+      return 1;
+   }
+}
+
 DLLEXPORT
 int vhpi_remove_cb(vhpiHandleT handle)
 {
@@ -689,7 +728,10 @@ int vhpi_remove_cb(vhpiHandleT handle)
    if (cb == NULL)
       return 1;
 
-   VHPI_MISSING;
+   VHPI_TRACE("cb.reason=%s", vhpi_cb_reason_str(cb->Reason));
+
+   int ret = disable_cb(cb);
+   return ret;
 }
 
 DLLEXPORT
@@ -707,7 +749,9 @@ int vhpi_disable_cb(vhpiHandleT cb_obj)
    if (cb == NULL)
       return 1;
 
-   VHPI_MISSING;
+   VHPI_TRACE("cb.reason=%s", vhpi_cb_reason_str(cb->Reason));
+
+   return disable_cb(cb);
 }
 
 DLLEXPORT
@@ -725,7 +769,7 @@ int vhpi_enable_cb(vhpiHandleT cb_obj)
    if (cb == NULL)
       return 1;
 
-   VHPI_MISSING;
+   return enable_cb(cb);
 }
 
 DLLEXPORT

test/vhpi/vhpi1.c
@@ -93,6 +93,18 @@ static void y_value_change(const vhpiCbDataT *cb_data)
    }
 }
 
+static void after_after_5ns(const vhpiCbDataT *cb_data)
+{
+   vhpi_printf("after_after_5ns callback!");
+   fail_if(1);
+}
+
+static void next_timestep(const vhpiCbDataT *cb_data)
+{
+   vhpi_printf("next_timestep callback!");
+   fail_if(1);
+}
+
 static void after_5ns(const vhpiCbDataT *cb_data)
 {
    vhpi_printf("after_5ns callback!");
@@ -125,6 +137,31 @@ static void after_5ns(const vhpiCbDataT *cb_data)
    };
    vhpi_register_cb(&cb_data2, 0);
    check_error();
+
+   vhpiTimeT time_1fs = {
+      .low = 1
+   };
+
+   vhpiCbDataT cb_data3 = {
+      .reason = vhpiCbAfterDelay,
+      .cb_rtn = after_after_5ns,
+      .time   = &time_1fs
+   };
+   vhpiHandleT cb3 = vhpi_register_cb(&cb_data3, vhpiReturnCb);
+   check_error();
+   fail_if(vhpi_disable_cb(cb3));
+
+   vhpiHandleT cb4 = vhpi_register_cb(&cb_data3, vhpiReturnCb);
+   check_error();
+   fail_if(vhpi_remove_cb(cb4));
+
+   vhpiCbDataT cb_data5 = {
+      .reason = vhpiCbNextTimeStep,
+      .cb_rtn = next_timestep,
+   };
+   vhpiHandleT cb5 = vhpi_register_cb(&cb_data5, vhpiReturnCb);
+   check_error();
+   fail_if(vhpi_remove_cb(cb5));
 }
 
 static void start_of_sim(const vhpiCbDataT *cb_data)

commit a716949283491f28289b19e03ecffcf786c88702

model: Don't add model->now when setting a timeout
We do this in the caller now.

Fixes: e9ec0c70 ("vhpi: Support enabling, disabling, and removing callbacks")

src/rt/model.c
@@ -3148,7 +3148,7 @@ void model_set_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
    assert(when > m->now);   // TODO: delta timeouts?
 
    void *e = tag_pointer(cb, EVENT_TIMEOUT);
-   heap_insert(m->eventq_heap, m->now + when, e);
+   heap_insert(m->eventq_heap, when, e);
 }
 
 rt_watch_t *model_set_event_cb(rt_model_t *m, rt_signal_t *s, sig_event_fn_t fn,