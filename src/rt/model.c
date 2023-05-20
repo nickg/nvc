nickg/nvc

commit d6e523a47f3dc1554b86a0b3b2c8dbc2e34f422c

vhpi: Free callbacks in remove
Callback handles should be released in vhpi_remove_cb. We can't just
free directly because callbacks may remove themselves while they
are executing. Instead, keep track of whether a callback is executing
and don't free the callback until after it's done.

src/vhpi/vhpi-model.c
@@ -235,12 +235,19 @@ typedef struct {
 
 DEF_CLASS(rootInst, vhpiRootInstK, designInstUnit.region.object);
 
+typedef enum {
+   CB_INACTIVE,
+   CB_ACTIVE,
+   CB_FREE_LATER
+} callback_status_t;
+
 typedef struct {
-   c_vhpiObject object;
-   vhpiStateT   State;
-   vhpiEnumT    Reason;
-   vhpiCbDataT  data;
-   uint64_t     when;
+   c_vhpiObject      object;
+   vhpiStateT        State;
+   vhpiEnumT         Reason;
+   vhpiCbDataT       data;
+   uint64_t          when;
+   callback_status_t status;
 } c_callback;
 
 DEF_CLASS(callback, vhpiCallbackK, object);
@@ -544,7 +551,15 @@ static void init_entityDecl(c_entityDecl *e, tree_t t)
 static void vhpi_do_callback(c_callback *cb)
 {
    if (cb->State == vhpiEnable) {
+      cb->status = CB_ACTIVE;
       (cb->data.cb_rtn)(&(cb->data));
+
+      if (cb->status == CB_FREE_LATER) {
+         free(cb);
+         return;
+      }
+      else
+         cb->status = CB_INACTIVE;
 
       if (!vhpi_is_repetitive(cb->Reason))
          cb->State = vhpiMature;
@@ -623,14 +638,20 @@ int vhpi_release_handle(vhpiHandleT handle)
    if (obj == NULL)
       return 1;
 
-   switch (obj->kind) {
-   case vhpiCallbackK:
-   case vhpiIteratorK:
-      free(obj);
-      return 0;
-   default:
+   c_callback *cb = is_callback(obj);
+   if (cb != NULL) {
+      if (cb->status != CB_INACTIVE)
+         cb->status = CB_FREE_LATER;
+      else
+         free(cb);
       return 0;
    }
+
+   c_iterator *it = is_iterator(obj);
+   if (it != NULL)
+      free(it);
+
+   return 0;
 }
 
 static int enable_cb(c_callback *cb)
@@ -749,6 +770,10 @@ int vhpi_remove_cb(vhpiHandleT handle)
    VHPI_TRACE("cb.reason=%s", vhpi_cb_reason_str(cb->Reason));
 
    int ret = disable_cb(cb);
+   if (cb->status != CB_INACTIVE)
+      cb->status = CB_FREE_LATER;
+   else
+      free(cb);
    return ret;
 }
 

commit e82ff9310758a3279474ca925b25b0fc8494bc33

vhpi: Fix use-after-free when removing callbacks
Callbacks can generally be in a few states:

- Disabled or mature
- Actively executing
- Waiting to execute

For all callbacks except for AfterDelay, when the callback is waiting to
execute then clearing it will prevent further execution. However,
AfterDelay callbacks can be absent from the eventq_heap in the driverq
workqueue awaiting execution. Clearing these callbacks will not prevent
them from executing.

To work around this, make model_clear_timeout_cb return a boolean
indicating whether a callback was actually cleared. If no callback was
cleared and the callback is not currently executing, then the callback
is in a workqueue. In this case, just free the callback later.
Otherwise, we have actually removed a callback and we can be sure that
it will not execute, so free it now.

Fixes: d6e523a4 ("vhpi: Free callbacks in remove")

src/rt/model.c
@@ -3252,7 +3252,7 @@ static bool eventq_delete_fn(uint64_t key, void *e, void *context)
    return false;
 }
 
-void model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
+bool model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
                             void *user)
 {
    timeout_params_t params = {
@@ -3261,7 +3261,7 @@ void model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
       .user = user,
    };
 
-   heap_delete(m->eventq_heap, eventq_delete_fn, &params);
+   return heap_delete(m->eventq_heap, eventq_delete_fn, &params);
 }
 
 void model_clear_event_cb(rt_model_t *m, rt_watch_t *w)

src/rt/model.h
@@ -41,7 +41,7 @@ void model_set_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
 void model_clear_global_cb(rt_model_t *m, rt_event_t event, rt_event_fn_t fn,
                            void *user);
 void model_clear_event_cb(rt_model_t *m, rt_watch_t *w);
-void model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
+bool model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
                             void *user);
 
 rt_model_t *get_model(void);

src/vhpi/vhpi-model.c
@@ -564,16 +564,14 @@ static void vhpi_do_callback(c_callback *cb)
       cb->status = CB_ACTIVE;
       (cb->data.cb_rtn)(&(cb->data));
 
-      if (cb->status == CB_FREE_LATER) {
-         free(cb);
-         return;
-      }
-      else
-         cb->status = CB_INACTIVE;
-
       if (!vhpi_is_repetitive(cb->Reason))
          cb->State = vhpiMature;
    }
+
+   if (cb->status == CB_FREE_LATER)
+      free(cb);
+   else
+      cb->status = CB_INACTIVE;
 }
 
 static void vhpi_timeout_cb(rt_model_t *m, void *user)
@@ -734,7 +732,7 @@ vhpiHandleT vhpi_register_cb(vhpiCbDataT *cb_data_p, int32_t flags)
    return (flags & vhpiReturnCb) ? handle_for(&(cb->object)) : NULL;
 }
 
-static int disable_cb(c_callback *cb)
+static bool disable_cb(c_callback *cb)
 {
    switch (cb->Reason) {
    case vhpiCbRepEndOfProcesses:
@@ -747,19 +745,18 @@ static int disable_cb(c_callback *cb)
    case vhpiCbNextTimeStep:
       model_clear_global_cb(model, vhpi_get_rt_event(cb->Reason),
                             vhpi_global_cb, cb);
-      return 0;
+      return true;
 
    case vhpiCbAfterDelay:
-      model_clear_timeout_cb(model, cb->when, vhpi_timeout_cb, cb);
-      return 0;
+      return model_clear_timeout_cb(model, cb->when, vhpi_timeout_cb, cb);
 
    case vhpiCbValueChange:
       model_clear_event_cb(model, cb->w);
-      return 0;
+      return true;
 
    default:
       assert(false);
-      return 1;
+      return true;
    }
 }
 
@@ -780,12 +777,13 @@ int vhpi_remove_cb(vhpiHandleT handle)
 
    VHPI_TRACE("cb.reason=%s", vhpi_cb_reason_str(cb->Reason));
 
-   int ret = disable_cb(cb);
-   if (cb->status != CB_INACTIVE)
-      cb->status = CB_FREE_LATER;
-   else
+   if (cb->State != vhpiEnable || (disable_cb(cb) && cb->status == CB_INACTIVE))
       free(cb);
-   return ret;
+   else {
+      cb->State = vhpiDisable;
+      cb->status = CB_FREE_LATER;
+   }
+   return 0;
 }
 
 DLLEXPORT
@@ -811,9 +809,9 @@ int vhpi_disable_cb(vhpiHandleT cb_obj)
       return 1;
    }
 
-   int ret = disable_cb(cb);
+   disable_cb(cb);
    cb->State = vhpiDisable;
-   return ret;
+   return 0;
 }
 
 DLLEXPORT