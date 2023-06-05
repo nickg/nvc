239bfc34f4349d2dfd589d7757829911627907f2
Brian Padalino <bpadalino@gmail.com>
2023-06-05 18:17:56 -0400

    Adding VHDL-2019 to OSVVM installation. (#622)

--- contrib/install-osvvm.tcl
+++ contrib/install-osvvm.tcl
@@ -2,7 +2,7 @@ source "Scripts/StartNVC.tcl"
 set ::osvvm::VhdlLibraryDirectory "${::env(NVC_INSTALL_DEST)}"
 set ::osvvm::VhdlLibrarySubdirectory "."
 
-foreach std {2008} {
+foreach std {2008 2019} {
     SetVHDLVersion $std
     source "OsvvmLibraries.pro"
 }
