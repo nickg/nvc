nickg/nvc

commit 2052a42fe56e4e11e004c3d9a650fb8bd2508a72

vhpi: Rework size calculation
IndexedNames represent a subset of the elements in a signal. Their size
is dependent on their type. In preparation for adding IndexedName
support, rework the size calculation to be based on the type of the
object instead of the width of the backing signal.

src/vhpi/vhpi-model.c
@@ -121,6 +121,7 @@ typedef struct tag_typeDecl {
    c_typeDecl     *BaseType;
    vhpiFormatT     format;
    const char     *map_str;
+   vhpiIntT        size;
    vhpiBooleanT    IsAnonymous;
    vhpiBooleanT    IsComposite;
    vhpiBooleanT    IsScalar;
@@ -560,6 +561,7 @@ static void init_scalarTypeDecl(c_scalarTypeDecl *d, tree_t t, type_t type)
 {
    init_typeDecl(&(d->typeDecl), t, type);
    d->typeDecl.IsScalar = true;
+   d->typeDecl.size = 1;
 }
 
 static void init_compositeTypeDecl(c_compositeTypeDecl *d, tree_t t, type_t type)
@@ -1201,21 +1203,12 @@ vhpiIntT vhpi_get(vhpiIntPropertyT property, vhpiHandleT handle)
 
    case vhpiSizeP:
       {
-         if (obj->kind != vhpiPortDeclK && obj->kind != vhpiSigDeclK) {
-            vhpi_error(vhpiInternal, &(obj->loc), "vhpiSizeP is only "
-                       "supported for signal and port objects");
-            return 0;
-         }
-
          c_objDecl *decl = cast_objDecl(obj);
          if (decl == NULL)
             return 0;
 
-         rt_signal_t *signal = vhpi_get_signal(decl);
-         if (signal == NULL)
-            return 0;
-
-         return signal_width(signal);
+         assert(!decl->Type->IsUnconstrained);
+         return decl->Type->size;
       }
 
    case vhpiArgcP:
@@ -1750,6 +1743,14 @@ static c_intRange *build_unconstrained()
    return ir;
 }
 
+static vhpiIntT range_len(c_intRange *ir)
+{
+   if (ir->LeftBound > ir->RightBound)
+      return ir->LeftBound - ir->RightBound + 1;
+   else
+      return ir->RightBound - ir->LeftBound + 1;
+}
+
 static c_typeDecl *cached_typeDecl(type_t type);
 
 static c_typeDecl *build_typeDecl(type_t type)
@@ -1814,6 +1815,7 @@ static c_typeDecl *build_typeDecl(type_t type)
             new_object(sizeof(c_recordTypeDecl), vhpiRecordTypeDeclK);
          init_compositeTypeDecl(&(td->composite), decl, type);
          td->NumFields = type_fields(type);
+         td->composite.typeDecl.size = td->NumFields;
          return &(td->composite.typeDecl);
       }
 
@@ -1824,12 +1826,14 @@ static c_typeDecl *build_typeDecl(type_t type)
                new_object(sizeof(c_arrayTypeDecl), vhpiArrayTypeDeclK);
             init_compositeTypeDecl(&(td->composite), decl, type);
             td->ElemType = cached_typeDecl(type_elem(type));
+            td->composite.typeDecl.size = td->ElemType->size;
 
             tree_t c = type_constraint(type, 0);
             assert(tree_subkind(c) == C_INDEX);
             td->NumDimensions = tree_ranges(c);
             for (int i = 0; i < td->NumDimensions; i++) {
                c_intRange *ir = build_int_range(tree_range(c, i));
+               td->composite.typeDecl.size *= range_len(ir);
                APUSH(td->Constraints, &(ir->range.object));
             }
 
@@ -1851,8 +1855,11 @@ static c_typeDecl *build_typeDecl(type_t type)
                fatal_trace("unsupported constraint subkind %d\n", tree_subkind(c));
 
             c_intRange *ir = build_int_range(tree_range(c, 0));
+            td->typeDecl.size = range_len(ir);
             APUSH(td->Constraints, &(ir->range.object));
          }
+         else
+            td->typeDecl.size = cached_typeDecl(type_base(type))->size;
 
          return &(td->typeDecl);
       }

commit 201c3f486acba44e143c4584de4619ac4f4c2132

vhpi: Fix incorrect range calculation
Null ranges have zero length. Detect them properly by basing the
direvction of the range on IsUp instead of whether LeftBound >
RightBound. The comparison in init_indexedName can stay the same since
it is guarded by build_indexedNames.

Fixes: 2052a42f ("vhpi: Rework size calculation")
Fixes: 9416eb6e ("vhpi: Add support for IndexedNames")

src/vhpi/vhpi-model.c
@@ -724,10 +724,10 @@ static void init_indexedName(c_indexedName *in, c_typeDecl *Type,
 
 static vhpiIntT range_len(c_intRange *ir)
 {
-   if (ir->LeftBound > ir->RightBound)
-      return ir->LeftBound - ir->RightBound + 1;
+   if (ir->range.IsUp)
+      return MAX(ir->RightBound - ir->LeftBound + 1, 0);
    else
-      return ir->RightBound - ir->LeftBound + 1;
+      return MAX(ir->LeftBound - ir->RightBound + 1, 0);
 }
 
 static void vhpi_build_indexedNames(vhpiObjectListT *IndexedNames, c_objDecl *simpleName,
@@ -763,6 +763,8 @@ static void vhpi_build_indexedNames(vhpiObjectListT *IndexedNames, c_objDecl *si
          return;
 
       lens[i] = range_len(ir);
+      if (lens[i] == 0)
+         return;
    }
 
    vhpiIntT *indices LOCAL = xcalloc_array(Constraints->count, sizeof(vhpiIntT));

test/regress/vhpi8.vhd
@@ -43,5 +43,6 @@ architecture test of vhpi8 is
    signal lv: std_logic_vector(0 to 3) := "UX01";
    signal sv: severity_vector(0 to 3) := (note, warning, error, failure);
    signal ev: enum_vector(0 to 3) := (e000, e001, e002, e003);
+   signal bv: bit_vector(3 to 0);
 begin
 end architecture;

test/vhpi/vhpi8.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static vhpiHandleT lv, sv, ev, lv2, sv2, ev2;
+static vhpiHandleT lv, sv, ev, bv, lv2, sv2, ev2;
 
 static void last_delta(const vhpiCbDataT *cb_data)
 {
@@ -133,6 +133,16 @@ static void start_of_sim(const vhpiCbDataT *cb_data)
    fail_unless(l[2] == 2);
    fail_unless(l[3] == 3);
 
+   lval.format = vhpiLogicVecVal;
+   vhpi_get_value(bv, &lval);
+   check_error();
+   fail_unless(lval.numElems == 0);
+
+   lval.bufSize = 0;
+   vhpi_put_value(bv, &lval, vhpiDepositPropagate);
+   check_error();
+   lval.bufSize = sizeof(l);
+
    lval2.format = vhpiLogicVal,
    vhpi_get_value(lv2, &lval2);
    check_error();
@@ -187,6 +197,16 @@ void vhpi8_startup(void)
    ev2 = vhpi_handle_by_index(vhpiIndexedNames, ev, 2);
    check_error();
    fail_if(ev2 == NULL);
+
+   bv = vhpi_handle_by_name("bv", root);
+   check_error();
+   fail_if(bv == NULL);
+
+   fail_unless(vhpi_get(vhpiSizeP, bv) == 0);
+   vhpiHandleT bv_names = vhpi_iterator(vhpiIndexedNames, bv);
+   check_error();
+   fail_if(bv_names == NULL);
+   fail_unless(vhpi_scan(bv_names) == NULL);
 
    vhpi_release_handle(root);
 }