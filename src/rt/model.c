nickg/nvc

commit d6e523a47f3dc1554b86a0b3b2c8dbc2e34f422c

vhpi: Free callbacks in remove
Callback handles should be released in vhpi_remove_cb. We can't just
free directly because callbacks may remove themselves while they
are executing. Instead, keep track of whether a callback is executing
and don't free the callback until after it's done.

src/vhpi/vhpi-model.c
@@ -235,12 +235,19 @@ typedef struct {
 
 DEF_CLASS(rootInst, vhpiRootInstK, designInstUnit.region.object);
 
+typedef enum {
+   CB_INACTIVE,
+   CB_ACTIVE,
+   CB_FREE_LATER
+} callback_status_t;
+
 typedef struct {
-   c_vhpiObject object;
-   vhpiStateT   State;
-   vhpiEnumT    Reason;
-   vhpiCbDataT  data;
-   uint64_t     when;
+   c_vhpiObject      object;
+   vhpiStateT        State;
+   vhpiEnumT         Reason;
+   vhpiCbDataT       data;
+   uint64_t          when;
+   callback_status_t status;
 } c_callback;
 
 DEF_CLASS(callback, vhpiCallbackK, object);
@@ -544,7 +551,15 @@ static void init_entityDecl(c_entityDecl *e, tree_t t)
 static void vhpi_do_callback(c_callback *cb)
 {
    if (cb->State == vhpiEnable) {
+      cb->status = CB_ACTIVE;
       (cb->data.cb_rtn)(&(cb->data));
+
+      if (cb->status == CB_FREE_LATER) {
+         free(cb);
+         return;
+      }
+      else
+         cb->status = CB_INACTIVE;
 
       if (!vhpi_is_repetitive(cb->Reason))
          cb->State = vhpiMature;
@@ -623,14 +638,20 @@ int vhpi_release_handle(vhpiHandleT handle)
    if (obj == NULL)
       return 1;
 
-   switch (obj->kind) {
-   case vhpiCallbackK:
-   case vhpiIteratorK:
-      free(obj);
-      return 0;
-   default:
+   c_callback *cb = is_callback(obj);
+   if (cb != NULL) {
+      if (cb->status != CB_INACTIVE)
+         cb->status = CB_FREE_LATER;
+      else
+         free(cb);
       return 0;
    }
+
+   c_iterator *it = is_iterator(obj);
+   if (it != NULL)
+      free(it);
+
+   return 0;
 }
 
 static int enable_cb(c_callback *cb)
@@ -749,6 +770,10 @@ int vhpi_remove_cb(vhpiHandleT handle)
    VHPI_TRACE("cb.reason=%s", vhpi_cb_reason_str(cb->Reason));
 
    int ret = disable_cb(cb);
+   if (cb->status != CB_INACTIVE)
+      cb->status = CB_FREE_LATER;
+   else
+      free(cb);
    return ret;
 }
 

commit 277759fd64766193f20573ad7734b9ff4ed4ac96

vhpi: Fix use-after-free when removing callbacks (part 2)
I made a first attempt at this back in commit a21a360e ("vhpi: Fix
use-after-free when removing callbacks"), but I didn't fix the global or
value callbacks. These can also have use-after-frees, which can be
triggered consistently with a pair of "mutually-removing" callbacks.
They are set up so that they trigger on the next cycle, and they try to
remove themselves. This fails without the fixes in this commit.

The main approach is to give rt_watch_t the same treatment that
c_callback got: adding a flag to free the watch after the callback
completes. Global callbacks are a bit simpler, and can mostly be treated
like timeout callbacks; if we don't find anything to remove, the
callback must be currently-enqueued, so we shouldn't free it.

Fixes: d6e523a4 ("vhpi: Free callbacks in remove")

src/rt/model.c
@@ -2514,9 +2514,13 @@ static void async_watch_callback(void *context, void *arg)
 
    assert(w->wakeable.pending);
    w->wakeable.pending = false;
+   bool free_later = w->wakeable.free_later;
 
    MODEL_ENTRY(m);
    (*w->fn)(m->now, w->signal, w, w->user_data);
+
+   if (free_later)
+      free(w);
 }
 
 static void async_timeout_callback(void *context, void *arg)
@@ -3228,19 +3232,24 @@ rt_watch_t *model_set_event_cb(rt_model_t *m, rt_signal_t *s, sig_event_fn_t fn,
    return w;
 }
 
-void model_clear_global_cb(rt_model_t *m, rt_event_t event, rt_event_fn_t fn,
+bool model_clear_global_cb(rt_model_t *m, rt_event_t event, rt_event_fn_t fn,
                            void *user)
 {
    assert(event < RT_LAST_EVENT);
 
    rt_callback_t **last = &m->global_cbs[event];
+   if (!*last)
+      return false;
+
    for (rt_callback_t *it = *last; it; last = &(it->next), it = it->next) {
       if (it->fn == fn && it->user == user) {
          *last = it->next;
          free(it);
-         return;
+         return true;
       }
    }
+
+   return false;
 }
 
 typedef struct {
@@ -3280,7 +3289,7 @@ bool model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
    return heap_delete(m->eventq_heap, eventq_delete_fn, &params);
 }
 
-void model_clear_event_cb(rt_model_t *m, rt_watch_t *w)
+bool model_clear_event_cb(rt_model_t *m, rt_watch_t *w)
 {
    rt_nexus_t *n = &(w->signal->nexus);
    for (int i = 0; i < w->signal->n_nexus; i++, n = n->chain)
@@ -3294,7 +3303,13 @@ void model_clear_event_cb(rt_model_t *m, rt_watch_t *w)
       }
    }
 
+   if (w->wakeable.pending) {
+      w->wakeable.free_later = true;
+      return false;
+   }
+
    free(w);
+   return true;
 }
 
 static void handle_interrupt_cb(jit_t *j, void *ctx)

src/rt/model.h
@@ -38,9 +38,9 @@ rt_watch_t *model_set_event_cb(rt_model_t *m, rt_signal_t *s, sig_event_fn_t fn,
 void model_set_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
                           void *user);
 
-void model_clear_global_cb(rt_model_t *m, rt_event_t event, rt_event_fn_t fn,
+bool model_clear_global_cb(rt_model_t *m, rt_event_t event, rt_event_fn_t fn,
                            void *user);
-void model_clear_event_cb(rt_model_t *m, rt_watch_t *w);
+bool model_clear_event_cb(rt_model_t *m, rt_watch_t *w);
 bool model_clear_timeout_cb(rt_model_t *m, uint64_t when, rt_event_fn_t fn,
                             void *user);
 

src/rt/structs.h
@@ -39,6 +39,7 @@ typedef struct {
    unsigned        pending : 1;
    unsigned        postponed : 1;
    unsigned        delayed : 1;
+   unsigned        free_later : 1;
 } rt_wakeable_t;
 
 typedef struct _rt_proc {

src/vhpi/vhpi-model.c
@@ -1103,16 +1103,15 @@ static bool disable_cb(c_callback *cb)
    case vhpiCbEndOfSimulation:
    case vhpiCbLastKnownDeltaCycle:
    case vhpiCbNextTimeStep:
-      model_clear_global_cb(model, vhpi_get_rt_event(cb->Reason),
-                            vhpi_global_cb, cb);
+      return model_clear_global_cb(model, vhpi_get_rt_event(cb->Reason),
+                                   vhpi_global_cb, cb);
       return true;
 
    case vhpiCbAfterDelay:
       return model_clear_timeout_cb(model, cb->when, vhpi_timeout_cb, cb);
 
    case vhpiCbValueChange:
-      model_clear_event_cb(model, cb->w);
-      return true;
+      return model_clear_event_cb(model, cb->w);
 
    default:
       assert(false);

test/vhpi/vhpi1.c
@@ -119,12 +119,15 @@ static void deferred_work(const vhpiCbDataT *cb_data)
    vhpi_enable_cb(defer_enable);
 }
 
-static vhpiHandleT autoremoving_cb;
+static vhpiHandleT mutual_cb1[2], mutual_cb2[2], mutual_cb3[2];
 
-static void autoremoving(const vhpiCbDataT *cb_data)
+static void mutual(const vhpiCbDataT *cb_data)
 {
-   vhpi_printf("autoremoving callback!");
-   vhpi_remove_cb(autoremoving_cb);
+   vhpi_printf("mutual callback!");
+
+   vhpiHandleT *cb = cb_data->user_data;
+   vhpi_remove_cb(cb[0]);
+   vhpi_remove_cb(cb[1]);
 }
 
 static void disabled_callback(const vhpiCbDataT *cb_data)
@@ -183,6 +186,13 @@ static void after_5ns(const vhpiCbDataT *cb_data)
    check_error();
    fail_if(vhpi_disable_cb(cb));
 
+   cb_data2.cb_rtn = mutual;
+   cb_data2.user_data = mutual_cb1;
+   mutual_cb1[0] = vhpi_register_cb(&cb_data2, vhpiReturnCb);
+   check_error();
+   mutual_cb1[1] = vhpi_register_cb(&cb_data2, vhpiReturnCb);
+   check_error();
+
    vhpiTimeT time = {
       .low = 1
    };
@@ -226,6 +236,13 @@ static void after_5ns(const vhpiCbDataT *cb_data)
    vhpi_register_cb(&cb_data4, 0);
    check_error();
 
+   cb_data4.cb_rtn = mutual;
+   cb_data4.user_data = mutual_cb2;
+   mutual_cb2[0] = vhpi_register_cb(&cb_data4, vhpiReturnCb);
+   check_error();
+   mutual_cb2[1] = vhpi_register_cb(&cb_data4, vhpiReturnCb);
+   check_error();
+
    vhpiCbDataT cb_data5 = {
       .reason = vhpiCbNextTimeStep,
       .cb_rtn = disabled_callback,
@@ -234,8 +251,11 @@ static void after_5ns(const vhpiCbDataT *cb_data)
    check_error();
    fail_if(vhpi_remove_cb(cb));
 
-   cb_data5.cb_rtn = autoremoving;
-   autoremoving_cb = vhpi_register_cb(&cb_data5, vhpiReturnCb);
+   cb_data5.cb_rtn = mutual;
+   cb_data5.user_data = mutual_cb3;
+   mutual_cb3[0] = vhpi_register_cb(&cb_data5, vhpiReturnCb);
+   check_error();
+   mutual_cb3[1] = vhpi_register_cb(&cb_data5, vhpiReturnCb);
    check_error();
 }
 