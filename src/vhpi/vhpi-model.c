nickg/nvc

commit fb8e5f5197135a88245b977e962487abb5e1eb03

vhpi: Support iterators
Add support for iterators. This is not the most useful, but at the very
least you can iterate over ports and declarations.

src/vhpi/vhpi-model.c
@@ -223,6 +223,7 @@ typedef struct {
 typedef struct {
    c_designInstUnit designInstUnit;
    vhpiObjectListT  ports;
+   vhpiObjectListT  regions;
 } c_rootInst;
 
 DEF_CLASS(rootInst, vhpiRootInstK, designInstUnit.region.object);
@@ -236,6 +237,14 @@ typedef struct {
 } c_callback;
 
 DEF_CLASS(callback, vhpiCallbackK, object);
+
+typedef struct {
+   c_vhpiObject     object;
+   vhpiObjectListT *list;
+   uint32_t         pos;
+} c_iterator;
+
+DEF_CLASS(iterator, vhpiIteratorK, object);
 
 static c_rootInst *rootInst = NULL;
 static shash_t    *strtab = NULL;
@@ -608,6 +617,7 @@ int vhpi_release_handle(vhpiHandleT handle)
 
    switch (obj->kind) {
    case vhpiCallbackK:
+   case vhpiIteratorK:
       free(obj);
       return 0;
    default:
@@ -914,19 +924,61 @@ vhpiHandleT vhpi_handle_by_index(vhpiOneToManyT itRel,
    }
 }
 
+static vhpiHandleT new_iterator(vhpiObjectListT *list)
+{
+   c_iterator *it = new_object(sizeof(c_iterator), vhpiIteratorK);
+   it->list = list;
+   it->pos = 0;
+   return handle_for(&(it->object));
+}
+
 DLLEXPORT
 vhpiHandleT vhpi_iterator(vhpiOneToManyT type, vhpiHandleT handle)
 {
    VHPI_TRACE("type=%s handle=%s", vhpi_one_to_many_str(type),
               handle_pp(handle));
 
-   VHPI_MISSING;
+   c_vhpiObject *obj = from_handle(handle);
+   if (obj == NULL)
+      return NULL;
+
+   c_rootInst *rootInst = is_rootInst(obj);
+   if (rootInst != NULL) {
+      switch (type) {
+      case vhpiPortDecls: return new_iterator(&(rootInst->ports));
+      case vhpiInternalRegions: return new_iterator(&(rootInst->regions));
+      default: break;
+      }
+   }
+
+   c_abstractRegion *region = is_abstractRegion(obj);
+   if (region != NULL) {
+      switch (type) {
+      case vhpiDecls: return new_iterator(&(region->decls));
+      case vhpiInternalRegions: return new_iterator(&(region->InternalRegions));
+      default: goto unsupported;
+      }
+   }
+
+unsupported:
+   fatal_trace("relation %s not supported in vhpi_iterator",
+               vhpi_one_to_many_str(type));
 }
 
 DLLEXPORT
 vhpiHandleT vhpi_scan(vhpiHandleT iterator)
 {
-   VHPI_MISSING;
+   c_vhpiObject *obj = from_handle(iterator);
+   if (obj == NULL)
+      return NULL;
+
+   c_iterator *it = cast_iterator(obj);
+   if (it->pos < it->list->count) {
+      vhpiHandleT handle = handle_for(it->list->items[it->pos++]);
+      return handle;
+   }
+
+   return NULL;
 }
 
 DLLEXPORT

test/vhpi/vhpi1.c
@@ -250,6 +250,16 @@ static void startup()
    vhpi_printf("root name is %s", root_name);
    vhpi_printf("root full name is %s", vhpi_get_str(vhpiFullNameP, root));
 
+   vhpiHandleT root_ports = vhpi_iterator(vhpiPortDecls, root);
+   fail_if(root_ports == NULL);
+   for (vhpiHandleT port = vhpi_scan(root_ports); port != NULL; port = vhpi_scan(root_ports))
+      vhpi_printf("root port is %s", vhpi_get_str(vhpiNameP, port));
+
+   vhpiHandleT root_decls = vhpi_iterator(vhpiDecls, root);
+   fail_if(root_decls == NULL);
+   for (vhpiHandleT decl = vhpi_scan(root_decls); decl != NULL; decl = vhpi_scan(root_decls))
+      vhpi_printf("root decl is %s", vhpi_get_str(vhpiNameP, decl));
+
    vhpiHandleT arch = vhpi_handle(vhpiDesignUnit, root);
    check_error();
    fail_if(arch == NULL);

commit a51331cd6eadba0ebfc16e75152dda81344cebb5

vhpi: Fix root region being an internal region of itself
The root region shouldn't contain itself. This can cause infinite
recursion in VHPI programs which try to recursively discover the
hierarchy.

Fixes: fb8e5f51 ("vhpi: Support iterators")

src/vhpi/vhpi-model.c
@@ -805,9 +805,6 @@ static bool init_iterator(c_iterator *it, vhpiOneToManyT type, c_vhpiObject *obj
    if (rootInst != NULL) {
       switch (type) {
       case vhpiPortDecls: it->list = &(rootInst->ports); return true;
-      case vhpiInternalRegions:
-         it->single = &(rootInst->designInstUnit.region.object);
-         return true;
       default: break;
       }
    }