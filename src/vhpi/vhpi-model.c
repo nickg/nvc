nickg/nvc

commit cdddab99b6c36ad4314935ffd50f9ad9841ba444

vhpi: Add get/set support for selected names
Inhomogenous types are create scopes instead of signals. Additionally,
inhomogenous arrays have nested scopes for their elements. Add the
necessary machinery to determine the scope for a name/objDecl, and look
up the signal at the final level.

In the style of vhpi7, assign a value to each element of the test
records, and try to change them. To avoid needing to repeat the same
code while recursing through records, we just print out the values and
check them against the golden output. We cheat a bit for
multidimensional arrays because array properties are checked properly in
vhpi7.

src/vhpi/vhpi-model.c
@@ -126,6 +126,7 @@ typedef struct tag_typeDecl {
    vhpiBooleanT    IsComposite;
    vhpiBooleanT    IsScalar;
    vhpiBooleanT    IsUnconstrained;
+   bool            homogeneous;
 } c_typeDecl;
 
 typedef struct {
@@ -195,6 +196,7 @@ typedef struct {
    vhpiObjectListT  IndexedNames;
    vhpiObjectListT  SelectedNames;
    rt_signal_t     *signal;
+   rt_scope_t      *scope;
    c_typeDecl      *Type;
    vhpiIntT         Access;
    vhpiStaticnessT  Staticness;
@@ -271,9 +273,11 @@ typedef struct {
 } c_name;
 
 typedef struct {
-   c_name     name;
-   c_objDecl *simpleName;
-   c_name    *Prefix;
+   c_name       name;
+   c_objDecl   *simpleName;
+   c_name      *Prefix;
+   rt_signal_t *signal;
+   rt_scope_t  *scope;
 } c_prefixedName;
 
 typedef struct {
@@ -688,6 +692,7 @@ static void init_typeDecl(c_typeDecl *d, tree_t t, type_t type)
    d->type   = type;
    d->format = vhpi_format_for_type(d->type, &d->map_str);
    d->IsUnconstrained = type_is_unconstrained(d->type);
+   d->homogeneous = type_is_homogeneous(d->type);
 }
 
 static void init_scalarTypeDecl(c_scalarTypeDecl *d, tree_t t, type_t type)
@@ -1097,7 +1102,7 @@ static void vhpi_global_cb(rt_model_t *m, void *user)
       vhpi_do_callback(cb);
 }
 
-static rt_scope_t *vhpi_get_scope(c_abstractRegion *region)
+static rt_scope_t *vhpi_get_scope_abstractRegion(c_abstractRegion *region)
 {
    if (region->scope)
       return region->scope;
@@ -1113,12 +1118,63 @@ static rt_scope_t *vhpi_get_scope(c_abstractRegion *region)
    return scope;
 }
 
-static rt_signal_t *vhpi_get_signal(c_objDecl *decl)
+static rt_scope_t *vhpi_get_scope_objDecl(c_objDecl *decl)
+{
+   if (decl->scope)
+      return decl->scope;
+
+   rt_scope_t *parent = vhpi_get_scope_abstractRegion(decl->decl.ImmRegion);
+   if (parent == NULL)
+      return NULL;
+
+   decl->scope = child_scope(parent, decl->decl.tree);
+   if (decl->scope == NULL)
+      vhpi_error(vhpiError, &(decl->decl.object.loc),
+                 "cannot find scope object for %s", decl->decl.Name);
+   return decl->scope;
+}
+
+static rt_scope_t *vhpi_get_scope_prefixedName(c_prefixedName *pn)
+{
+   if (pn->scope)
+      return pn->scope;
+
+   rt_scope_t *parent;
+   if (pn->Prefix) {
+      c_prefixedName *ppn = is_prefixedName(&(pn->Prefix->expr.object));
+      assert(ppn != NULL);
+
+      parent = vhpi_get_scope_prefixedName(ppn);
+   }
+   else
+      parent = vhpi_get_scope_objDecl(pn->simpleName);
+
+   if (parent == NULL)
+      return NULL;
+
+   c_vhpiObject *obj = &(pn->name.expr.object);
+   c_indexedName *in = is_indexedName(obj);
+   c_selectedName *sn = is_selectedName(obj);
+   if (in != NULL)
+      pn->scope = child_scope_at(parent, in->BaseIndex);
+   else if (sn != NULL)
+      pn->scope = child_scope(parent, sn->Suffix->decl.tree);
+   else
+      fatal_trace("class kind %s not supported in %s",
+                  vhpi_class_str(obj->kind), __func__);
+
+   if (pn->scope == NULL)
+      vhpi_error(vhpiError, &(obj->loc), "cannot find scope object for %s",
+                 pn->name.Name);
+   return pn->scope;
+}
+
+static rt_signal_t *vhpi_get_signal_objDecl(c_objDecl *decl)
 {
    if (decl->signal != NULL)
       return decl->signal;
 
-   rt_scope_t *scope = vhpi_get_scope(decl->decl.ImmRegion);
+   rt_scope_t *scope = vhpi_get_scope_abstractRegion(decl->decl.ImmRegion);
    if (scope == NULL)
       return NULL;
 
@@ -1131,6 +1187,42 @@ static rt_signal_t *vhpi_get_signal(c_objDecl *decl)
 
    decl->signal = signal;
    return signal;
+}
+
+static rt_signal_t *vhpi_get_signal_prefixedName(c_prefixedName *pn)
+{
+   if (pn->signal)
+      return pn->signal;
+
+   rt_scope_t *scope = NULL;
+   if (pn->Prefix) {
+      c_prefixedName *ppn = is_prefixedName(&(pn->Prefix->expr.object));
+      assert(ppn != NULL);
+
+      if (pn->Prefix->expr.Type->homogeneous)
+         pn->signal = vhpi_get_signal_prefixedName(ppn);
+      else
+         scope = vhpi_get_scope_prefixedName(ppn);
+   }
+   else {
+      if (pn->simpleName->Type->homogeneous)
+         pn->signal = vhpi_get_signal_objDecl(pn->simpleName);
+      else
+         scope = vhpi_get_scope_objDecl(pn->simpleName);
+   }
+
+   c_vhpiObject *obj = &(pn->name.expr.object);
+   if (scope) {
+      c_selectedName *sn = is_selectedName(obj);
+      assert(sn != NULL);
+
+      pn->signal = find_signal(scope, sn->Suffix->decl.tree);
+   }
+
+   if (pn->signal == NULL)
+      vhpi_error(vhpiError, &(obj->loc), "cannot find signal object for %s",
+                 pn->name.Name);
+   return pn->signal;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1194,7 +1286,7 @@ static int enable_cb(c_callback *cb)
          if (decl == NULL)
             return 1;
 
-         rt_signal_t *signal = vhpi_get_signal(decl);
+         rt_signal_t *signal = vhpi_get_signal_objDecl(decl);
          if (signal == NULL)
             return 1;
 
@@ -1871,7 +1963,7 @@ vhpiPhysT vhpi_get_phys(vhpiPhysPropertyT property,
          if (td == NULL)
             return invalid;
 
-         rt_signal_t *signal = vhpi_get_signal(decl);
+         rt_signal_t *signal = vhpi_get_signal_objDecl(decl);
          if (signal == NULL)
             return invalid;
 
@@ -1913,21 +2005,21 @@ int vhpi_get_value(vhpiHandleT expr, vhpiValueT *value_p)
    if (obj == NULL)
       return -1;
 
-   int offset;
+   int offset = 0;
    c_objDecl *decl;
    c_typeDecl *td;
-   c_indexedName *in = is_indexedName(obj);
-   if (in != NULL) {
-      decl = in->prefixedName.simpleName;
-      td = in->prefixedName.name.expr.Type;
-      offset = in->offset;
+   c_prefixedName *pn = is_prefixedName(obj);
+   if (pn != NULL) {
+      td = pn->name.expr.Type;
+      c_indexedName *in = is_indexedName(obj);
+      if (in)
+         offset = in->offset;
    }
    else {
       decl = cast_objDecl(obj);
       if (decl == NULL)
          return 1;
       td = decl->Type;
-      offset = 0;
    }
 
    if (td->format == (vhpiFormatT)-1) {
@@ -1936,7 +2028,12 @@ int vhpi_get_value(vhpiHandleT expr, vhpiValueT *value_p)
       return -1;
    }
 
-   rt_signal_t *signal = vhpi_get_signal(decl);
+   rt_signal_t *signal;
+   if (pn != NULL)
+      signal = vhpi_get_signal_prefixedName(pn);
+   else
+      signal = vhpi_get_signal_objDecl(decl);
+
    if (signal == NULL)
       return -1;
 
@@ -2080,21 +2177,23 @@ int vhpi_put_value(vhpiHandleT handle,
    if (obj == NULL)
       return 1;
 
-   int offset;
-   c_objDecl *decl;
-   c_indexedName *in = is_indexedName(obj);
-   if (in != NULL) {
-      decl = in->prefixedName.simpleName;
-      offset = in->offset;
+   int offset = 0;
+   rt_signal_t *signal;
+   c_prefixedName *pn = is_prefixedName(obj);
+   if (pn != NULL) {
+      signal = vhpi_get_signal_prefixedName(pn);
+      c_indexedName *in = is_indexedName(obj);
+      if (in)
+         offset = in->offset;
    }
    else {
-      decl = cast_objDecl(obj);
+      c_objDecl *decl = cast_objDecl(obj);
       if (decl == NULL)
          return 1;
-      offset = 0;
+
+      signal = vhpi_get_signal_objDecl(decl);
    }
 
-   rt_signal_t *signal = vhpi_get_signal(decl);
    if (signal == NULL)
       return 1;
 

src/rt/model.c
@@ -824,6 +824,11 @@ rt_scope_t *child_scope(rt_scope_t *scope, tree_t decl)
    return NULL;
 }
 
+rt_scope_t *child_scope_at(rt_scope_t *scope, int index)
+{
+   return (rt_scope_t *)scope->children->items[index];
+}
+
 const void *signal_value(rt_signal_t *s)
 {
    return s->shared.data;

src/rt/model.h
@@ -51,6 +51,7 @@ cover_tagging_t *get_coverage(rt_model_t *m);
 
 rt_scope_t *find_scope(rt_model_t *m, tree_t container);
 rt_scope_t *child_scope(rt_scope_t *scope, tree_t decl);
+rt_scope_t *child_scope_at(rt_scope_t *scope, int index);
 rt_signal_t *find_signal(rt_scope_t *scope, tree_t decl);
 rt_proc_t *find_proc(rt_scope_t *scope, tree_t proc);
 

test/regress/vhpi5.vhd
@@ -8,12 +8,34 @@ architecture test of vhpi5 is
       c: integer_vector(0 to 1);
    end record;
 
-   signal m: rec;
+   function init_rec(base: integer) return rec is
+      variable m: rec;
+   begin
+      m.a := (base + 0) * 16;
+      m.b := (base + 1) * 16;
+      for i in m.c'range loop
+         m.c(i) := (base + 2) * 16 + i;
+      end loop;
+      return m;
+   end;
+
+   signal m: rec := init_rec(0);
 
    -- Array of records
    type rec_matrix is array(0 to 6, 0 to 4) of rec;
 
-   signal n: rec_matrix;
+   function init_rec_matrix return rec_matrix is
+      variable n: rec_matrix;
+   begin
+      for i in n'range(1) loop
+         for j in n'range(2) loop
+            n(i, j) := init_rec(i * 256 + j * 16);
+         end loop;
+      end loop;
+      return n;
+   end;
+
+   signal n: rec_matrix := init_rec_matrix;
 
    -- Record with array of records
    type rec_vector is array(natural range <>) of rec;
@@ -23,6 +45,23 @@ architecture test of vhpi5 is
       e, f, g: rec_vector(0 to 4);
    end record;
 
-   signal o: rec2;
+   function init_rec2 return rec2 is
+      variable o: rec2;
+   begin
+      o.a := 0 * 4096;
+      o.b := 1 * 4096;
+      for i in o.c'range loop
+         o.c(i) := 2 * 4096 + i * 256;
+         o.d(i) := 3 * 4096 + i * 256;
+      end loop;
+      for i in o.e'range loop
+         o.e(i) := init_rec(4 * 256 + i * 16);
+         o.f(i) := init_rec(5 * 256 + i * 16);
+         o.g(i) := init_rec(6 * 256 + i * 16);
+      end loop;
+      return o;
+   end;
+
+   signal o: rec2 := init_rec2;
 begin
 end architecture;

test/vhpi/vhpi5.c
@@ -7,13 +7,60 @@
 
 static vhpiHandleT m, n, o;
 
-void check_recursive(vhpiHandleT parent)
+static vhpiIntT swan(vhpiIntT x)
+{
+   return (x & 0xf000) >> 12 |
+          (x & 0x0f00) >> 4 |
+          (x & 0x00f0) << 4 |
+          (x & 0x000f) << 12;
+}
+
+void delta_recursive(vhpiHandleT parent, int base, int scale)
+{
+   int i = 0;
+
+   vhpiHandleT children = vhpi_iterator(vhpiSelectedNames, parent);
+   for (vhpiHandleT child = vhpi_scan(children);
+        child;
+        child = vhpi_scan(children), i++)
+      delta_recursive(child, base + i * scale, scale / 16);
+
+   children = vhpi_iterator(vhpiIndexedNames, parent);
+   for (vhpiHandleT child = vhpi_scan(children);
+        child;
+        child = vhpi_scan(children), i++) {
+      if (parent == n)
+         delta_recursive(child, base + i / 5 * scale + i % 5 * scale / 16,
+                         scale / 256);
+      else
+         delta_recursive(child, base + i * scale, scale / 16);
+   }
+
+   if (!i) {
+      vhpiValueT val = {
+         .format = vhpiIntVal,
+      };
+
+      vhpi_get_value(parent, &val);
+      check_error();
+      vhpi_printf("%-14s = %.04x", vhpi_get_str(vhpiNameP, parent),
+                  val.value.intg);
+      fail_unless(val.value.intg == swan(base));
+   }
+}
+
+static void last_delta(const vhpiCbDataT *cb_data)
+{
+   delta_recursive(m, 0, 16);
+   delta_recursive(n, 0, 4096);
+   delta_recursive(o, 0, 4096);
+}
+
+void start_recursive(vhpiHandleT parent, int base, int scale)
 {
    int i = 0;
    char name[64];
    const vhpiCharT *parent_name = vhpi_get_str(vhpiNameP, parent);
-
-   vhpi_printf("checking %s", parent_name);
 
    vhpiHandleT children = vhpi_iterator(vhpiSelectedNames, parent);
    for (vhpiHandleT child = vhpi_scan(children);
@@ -28,14 +75,41 @@ void check_recursive(vhpiHandleT parent)
                vhpi_get_str(vhpiNameP, suffix));
       fail_if(strcmp(name, (char *)vhpi_get_str(vhpiNameP, child)));
 
-      check_recursive(child);
+      start_recursive(child, base + i * scale, scale / 16);
    }
 
    children = vhpi_iterator(vhpiIndexedNames, parent);
    for (vhpiHandleT child = vhpi_scan(children);
         child;
-        child = vhpi_scan(children))
-      check_recursive(child);
+        child = vhpi_scan(children), i++) {
+      if (parent == n)
+         start_recursive(child, base + i / 5 * scale + i % 5 * scale / 16,
+                         scale / 256);
+      else
+         start_recursive(child, base + i * scale, scale / 16);
+   }
+
+   if (!i) {
+      vhpiValueT val = {
+         .format = vhpiIntVal,
+      };
+
+      vhpi_get_value(parent, &val);
+      check_error();
+      vhpi_printf("%-14s = %.04x", parent_name, val.value.intg);
+      fail_unless(val.value.intg == base);
+
+      val.value.intg = swan(val.value.intg);
+      vhpi_put_value(parent, &val, vhpiDepositPropagate);
+      check_error();
+   }
+}
+
+static void start_of_sim(const vhpiCbDataT *cb_data)
+{
+   start_recursive(m, 0, 16);
+   start_recursive(n, 0, 4096);
+   start_recursive(o, 0, 4096);
 }
 
 void vhpi5_startup(void)
@@ -80,9 +154,17 @@ void vhpi5_startup(void)
    check_error();
    fail_if(n == NULL);
 
-   check_recursive(m);
-   check_recursive(n);
-   check_recursive(o);
+   vhpiCbDataT cb_data = {
+      .reason = vhpiCbStartOfSimulation,
+      .cb_rtn = start_of_sim,
+   };
+   vhpi_register_cb(&cb_data, 0);
+   check_error();
+
+   cb_data.reason = vhpiCbLastKnownDeltaCycle;
+   cb_data.cb_rtn = last_delta;
+   vhpi_register_cb(&cb_data, 0);
+   check_error();
 
    vhpi_release_handle(root);
 }

commit 5c0a0e0158432a2f50dc919dccdd4ba19f2add45

vhpi: Fix error message in vhpi_get_value
This error message unconditionally references decl, but sometimes all we
have is pn. Fix it.

Fixes: cdddab99 ("vhpi: Add get/set support for selected names")

src/vhpi/vhpi-model.c
@@ -2082,7 +2082,7 @@ int vhpi_get_value(vhpiHandleT expr, vhpiValueT *value_p)
    else if (value_p->format != td->format) {
       vhpi_error(vhpiError, &(obj->loc), "invalid format %d for "
                  "object %s: expecting %d", value_p->format,
-                 decl->decl.Name, td->format);
+                 pn ? pn->name.Name : decl->decl.Name, td->format);
       return -1;
    }
 