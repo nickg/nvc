3a32fb7eb20f7041d7ae1ded7beb16ca21a990de
Sean Anderson <seanga2@gmail.com>
2023-06-21 20:42:18 -0400

    vhpi: Add support for selected names
    The other major type of prefixed names is selected names. Add support
    for them. In many ways they are simpler than indexed names, since we
    don't have to keep track of multiple indices. Getting/setting is not
    supported, but it will be addressed in the next commit.

--- test/regress/vhpi5.vhd
+++ test/regress/vhpi5.vhd
@@ -2,11 +2,27 @@ entity vhpi5 is
 end entity;
 
 architecture test of vhpi5 is
+   -- Simple record
    type rec is record
       a, b: integer;
       c: integer_vector(0 to 1);
    end record;
 
    signal m: rec;
+
+   -- Array of records
+   type rec_matrix is array(0 to 6, 0 to 4) of rec;
+
+   signal n: rec_matrix;
+
+   -- Record with array of records
+   type rec_vector is array(natural range <>) of rec;
+   type rec2 is record
+      a, b: integer;
+      c, d: integer_vector(0 to 4);
+      e, f, g: rec_vector(0 to 4);
+   end record;
+
+   signal o: rec2;
 begin
 end architecture;
