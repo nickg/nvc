1f38dfd6adf0d9ce518e2456cbba9f23e6dd70f6
Nick Gasson <nick@nickg.me.uk>
2023-07-14 10:40:35 +0100

    Update Quartus installation script for 22.1

--- contrib/install-quartus.sh
+++ contrib/install-quartus.sh
@@ -5,6 +5,9 @@
 
 . $(dirname $BASH_SOURCE)/functions.sh
 
+GLOBAL_OPTS="-M 128m"
+A_OPTS="--relaxed"
+
 if [ -n "$MSYSTEM" ]; then
   QUARTUS_ROOTDIR=$(cygpath -u $QUARTUS_ROOTDIR)
 fi
@@ -19,76 +22,63 @@ directory.  For example:
 
 EOF
   exit 1
-elif [ ! -d "$QUARTUS_ROOTDIR/eda/sim_lib" ]; then
-  cat >&2 <<EOF
-$QUARTUS_ROOTDIR/eda/sim_lib not found
+elif [ -d "$QUARTUS_ROOTDIR/eda/sim_lib" ]; then
+  src=$QUARTUS_ROOTDIR/eda/sim_lib
 
-Ensure QUARTUS_ROOTDIR points at a valid Quartus installation directory.
+  echo "Using Quartus installation in $QUARTUS_ROOTDIR"
+  echo
 
-EOF
-  exit 1
-fi
+  #
+  # Old Quartus directory layout
+  #
 
-echo "Using Quartus installation in $QUARTUS_ROOTDIR"
-echo
+  for STD in 1993 2008; do
 
-src=$QUARTUS_ROOTDIR/eda/sim_lib
-
-GLOBAL_OPTS="-M 128m"
-A_OPTS="--relaxed"
-
-for STD in 1993 2008; do
-
-  analyse_list altera$(std_suffix $STD) <<EOF
+    analyse_list altera$(std_suffix $STD) <<EOF
 $src/altera_primitives_components.vhd
 $src/altera_primitives.vhd
 EOF
 
-  analyse_list altera_mf$(std_suffix $STD) <<EOF
+    analyse_list altera_mf$(std_suffix $STD) <<EOF
 $src/altera_mf_components.vhd
 $src/altera_mf.vhd
 EOF
 
-  analyse_list lpm$(std_suffix $STD) <<EOF
+    analyse_list lpm$(std_suffix $STD) <<EOF
 $src/220pack.vhd
 $src/220model.vhd
 EOF
 
-  analyse_list lpm$(std_suffix $STD) <<EOF
-$src/220pack.vhd
-$src/220model.vhd
-EOF
-
-  analyse_list altera_lnsim$(std_suffix $STD) <<EOF
+    analyse_list altera_lnsim$(std_suffix $STD) <<EOF
 $src/altera_lnsim_components.vhd
 EOF
 
-  analyse_list sgate$(std_suffix $STD) <<EOF
+    analyse_list sgate$(std_suffix $STD) <<EOF
 $src/sgate_pack.vhd
 $src/sgate.vhd
 EOF
 
-  analyse_list max$(std_suffix $STD) <<EOF
+    analyse_list max$(std_suffix $STD) <<EOF
 $src/max_atoms.vhd
 $src/max_components.vhd
 EOF
 
-  analyse_list maxii$(std_suffix $STD) <<EOF
+    analyse_list maxii$(std_suffix $STD) <<EOF
 $src/maxii_atoms.vhd
 $src/maxii_components.vhd
 EOF
 
-  analyse_list maxv$(std_suffix $STD) <<EOF
+    analyse_list maxv$(std_suffix $STD) <<EOF
 $src/maxv_atoms.vhd
 $src/maxv_components.vhd
 EOF
 
-  analyse_list fiftyfivenm$(std_suffix $STD) <<EOF
+    analyse_list fiftyfivenm$(std_suffix $STD) <<EOF
 $src/fiftyfivenm_atoms.vhd
 $src/fiftyfivenm_components.vhd
 EOF
 
-  analyse_list cycloneiv$(std_suffix $STD) <<EOF
+    analyse_list cycloneiv$(std_suffix $STD) <<EOF
 $src/cycloneiv_atoms.vhd
 $src/cycloneiv_components.vhd
 $src/cycloneiv_hssi_components.vhd
@@ -97,35 +87,139 @@ $src/cycloneive_atoms.vhd
 $src/cycloneive_components.vhd
 EOF
 
-# produces compilation error
-#  analyse_list cycloneiv_pcie_hip$(std_suffix $STD) <<EOF
-#$src/cycloneiv_pcie_hip_components.vhd
-#$src/cycloneiv_pcie_hip_atoms.vhd
-#EOF
+    # produces compilation error
+    #  analyse_list cycloneiv_pcie_hip$(std_suffix $STD) <<EOF
+    #$src/cycloneiv_pcie_hip_components.vhd
+    #$src/cycloneiv_pcie_hip_atoms.vhd
+    #EOF
 
-  analyse_list cyclonev$(std_suffix $STD) <<EOF
+    analyse_list cyclonev$(std_suffix $STD) <<EOF
 $src/cyclonev_atoms.vhd
 $src/cyclonev_components.vhd
 $src/cyclonev_hssi_components.vhd
 $src/cyclonev_hssi_atoms.vhd
 EOF
 
-  analyse_list cyclone10lp$(std_suffix $STD) <<EOF
+    analyse_list cyclone10lp$(std_suffix $STD) <<EOF
 $src/cyclone10lp_atoms.vhd
 $src/cyclone10lp_components.vhd
 EOF
 
-  analyse_list arriaii$(std_suffix $STD) <<EOF
+    analyse_list arriaii$(std_suffix $STD) <<EOF
 $src/arriaii_atoms.vhd
 $src/arriaii_components.vhd
 $src/arriaii_hssi_components.vhd
 $src/arriaii_hssi_atoms.vhd
 EOF
 
-# produces compilation error
-#  analyse_list arriaii_pcie_hip$(std_suffix $STD) <<EOF
-#$src/arriaii_pcie_hip_components.vhd
-#$src/arriaii_pcie_hip_atoms.vhd
+    # produces compilation error
+    #  analyse_list arriaii_pcie_hip$(std_suffix $STD) <<EOF
+    #$src/arriaii_pcie_hip_components.vhd
+    #$src/arriaii_pcie_hip_atoms.vhd
+    #EOF
+  done
+
+elif [ -d "$QUARTUS_ROOTDIR/questa_fse/intel/vhdl/src/altera" ]; then
+  src=$QUARTUS_ROOTDIR/questa_fse/intel/vhdl/src/
+
+  echo "Using Quartus installation in $QUARTUS_ROOTDIR"
+  echo
+
+  #
+  # Quartus 22.1 and later
+  #
+
+  for STD in 1993 2008; do
+
+    analyse_list altera$(std_suffix $STD) <<EOF
+$src/altera/altera_primitives_components.vhd
+$src/altera/altera_primitives.vhd
+EOF
+
+    analyse_list altera_mf$(std_suffix $STD) <<EOF
+$src/altera_mf/altera_mf_components.vhd
+$src/altera_mf/altera_mf.vhd
+EOF
+
+    analyse_list lpm$(std_suffix $STD) <<EOF
+$src/220model/220pack.vhd
+$src/220model/220model.vhd
+EOF
+
+    analyse_list altera_lnsim$(std_suffix $STD) <<EOF
+$src/altera_lnsim/altera_lnsim_components.vhd
+EOF
+
+    analyse_list sgate$(std_suffix $STD) <<EOF
+$src/sgate/sgate_pack.vhd
+$src/sgate/sgate.vhd
+EOF
+
+    analyse_list maxii$(std_suffix $STD) <<EOF
+$src/maxii/maxii_atoms.vhd
+$src/maxii/maxii_components.vhd
+EOF
+
+    analyse_list maxv$(std_suffix $STD) <<EOF
+$src/maxv/maxv_atoms.vhd
+$src/maxv/maxv_components.vhd
+EOF
+
+    analyse_list fiftyfivenm$(std_suffix $STD) <<EOF
+$src/fiftyfivenm/fiftyfivenm_atoms.vhd
+$src/fiftyfivenm/fiftyfivenm_components.vhd
+EOF
+
+    analyse_list cycloneiv$(std_suffix $STD) <<EOF
+$src/cycloneiv/cycloneiv_atoms.vhd
+$src/cycloneiv/cycloneiv_components.vhd
+$src/cycloneiv_hssi/cycloneiv_hssi_components.vhd
+$src/cycloneiv_hssi/cycloneiv_hssi_atoms.vhd
+$src/cycloneive/cycloneive_atoms.vhd
+$src/cycloneive/cycloneive_components.vhd
+EOF
+
+    # produces compilation error
+#    analyse_list cycloneiv_pcie_hip$(std_suffix $STD) <<EOF
+#$src/cycloneiv_pcie_hip/cycloneiv_pcie_hip_components.vhd
+#$src/cycloneiv_pcie_hip/cycloneiv_pcie_hip_atoms.vhd
 #EOF
 
-done
+    analyse_list cyclonev$(std_suffix $STD) <<EOF
+$src/cyclonev/cyclonev_atoms.vhd
+$src/cyclonev/cyclonev_components.vhd
+$src/cyclonev_hssi/cyclonev_hssi_components.vhd
+$src/cyclonev_hssi/cyclonev_hssi_atoms.vhd
+EOF
+
+    analyse_list cyclone10lp$(std_suffix $STD) <<EOF
+$src/cyclone10lp/cyclone10lp_atoms.vhd
+$src/cyclone10lp/cyclone10lp_components.vhd
+EOF
+
+    analyse_list arriaii$(std_suffix $STD) <<EOF
+$src/arriaii/arriaii_atoms.vhd
+$src/arriaii/arriaii_components.vhd
+$src/arriaii_hssi/arriaii_hssi_components.vhd
+$src/arriaii_hssi/arriaii_hssi_atoms.vhd
+EOF
+
+    # produces compilation error
+#    analyse_list arriaii_pcie_hip$(std_suffix $STD) <<EOF
+#$src/arriaii_pcie_hip/arriaii_pcie_hip_components.vhd
+#$src/arriaii_pcie_hip/arriaii_pcie_hip_atoms.vhd
+#EOF
+
+  done
+
+else
+  cat >&2 <<EOF
+Could not find Altera simulation library sources in:
+  $QUARTUS_ROOTDIR/eda/sim_lib
+  $QUARTUS_ROOTDIR/questa_fse/intel/vhdl/src/altera
+
+Ensure QUARTUS_ROOTDIR points at a valid Quartus installation directory.
+
+EOF
+  exit 1
+fi
