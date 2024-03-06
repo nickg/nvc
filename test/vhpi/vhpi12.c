nickg/nvc

commit b52c57635f1a48d0a5d9df224fb72366eacc2439

Use a pseudo-source for scheduled deposits

src/rt/model.c
@@ -66,11 +66,6 @@ typedef struct _memblock {
 } memblock_t;
 
 typedef struct {
-   rt_nexus_t *nexus;
-   uint8_t     data[0];
-} rt_deposit_t;
-
-typedef struct {
    waveform_t    *free_waveforms;
    tlab_t         tlab;
    tlab_t         spare_tlab;
@@ -165,7 +160,7 @@ static void async_update_property(rt_model_t *m, void *arg);
 static void async_update_driver(rt_model_t *m, void *arg);
 static void async_fast_driver(rt_model_t *m, void *arg);
 static void async_fast_all_drivers(rt_model_t *m, void *arg);
-static void async_update_driving(rt_model_t *m, void *arg);
+static void async_force_release(rt_model_t *m, void *arg);
 static void async_deposit(rt_model_t *m, void *arg);
 static void async_transfer_signal(rt_model_t *m, void *arg);
 
@@ -607,6 +602,7 @@ static void cleanup_nexus(rt_model_t *m, rt_nexus_t *n)
 
       case SOURCE_PORT:
       case SOURCE_FORCING:
+      case SOURCE_DEPOSIT:
          break;
       }
    }
@@ -873,7 +869,7 @@ static void deltaq_insert_driver(rt_model_t *m, uint64_t delta,
 
 static void deltaq_insert_force_release(rt_model_t *m, rt_nexus_t *nexus)
 {
-   deferq_do(&m->delta_driverq, async_update_driving, nexus);
+   deferq_do(&m->delta_driverq, async_force_release, nexus);
    m->next_is_delta = true;
 }
 
@@ -1274,6 +1270,11 @@ static rt_source_t *add_source(rt_model_t *m, rt_nexus_t *n, source_kind_t kind)
    case SOURCE_FORCING:
       src->u.forcing = alloc_value(m, n);
       break;
+
+   case SOURCE_DEPOSIT:
+      src->u.deposit.nexus = n;
+      src->u.deposit.value = alloc_value(m, n);
+      break;
    }
 
    return src;
@@ -1515,6 +1516,11 @@ static void clone_source(rt_model_t *m, rt_nexus_t *nexus,
 
    case SOURCE_FORCING:
       split_value(nexus, &(new->u.forcing), &(old->u.forcing), offset);
+      break;
+
+   case SOURCE_DEPOSIT:
+      split_value(nexus, &(new->u.deposit.value), &(old->u.deposit.value),
+                  offset);
       break;
    }
 }
@@ -1773,6 +1779,7 @@ static void *source_value(rt_nexus_t *nexus, rt_source_t *src)
             + nexus->signal->offset + nexus->offset;
 
    case SOURCE_FORCING:
+   case SOURCE_DEPOSIT:
       assert(src->disconnected);
       return NULL;
    }
@@ -1874,27 +1881,40 @@ static void *call_resolution(rt_nexus_t *nexus, res_memo_t *r, int nonnull)
    }
 }
 
-static rt_source_t *get_forcing_source(rt_model_t *m, rt_nexus_t *n)
+static rt_source_t *get_pseudo_source(rt_model_t *m, rt_nexus_t *n,
+                                      source_kind_t kind)
 {
+   assert(kind == SOURCE_FORCING || kind == SOURCE_DEPOSIT);
+
    if (n->n_sources > 0) {
       for (rt_source_t *s = &(n->sources); s; s = s->chain_input) {
-         if (s->tag == SOURCE_FORCING)
+         if (s->tag == kind)
             return s;
       }
    }
 
-   return add_source(m, n, SOURCE_FORCING);
+   return add_source(m, n, kind);
 }
 
 static void *driving_value(rt_nexus_t *nexus)
 {
-   // Algorithm for driving values is in LRM 08 section 14.7.7.2
+   // Algorithm for driving values is in LRM 08 section 14.7.3.2
 
    // If S is driving-value forced, the driving value of S is unchanged
    // from its previous value; no further steps are required.
    if (unlikely(nexus->flags & NET_F_FORCED)) {
-      rt_source_t *src = get_forcing_source(get_model(), nexus);
+      rt_source_t *src = get_pseudo_source(get_model(), nexus, SOURCE_FORCING);
       return value_ptr(nexus, &(src->u.forcing));
+   }
+
+   // If a driving-value deposit is scheduled for S or for a signal of
+   // which S is a subelement, the driving value of S is the driving
+   // deposit value for S or the element of the driving deposit value
+   // for the signal of which S is a subelement, as appropriate.
+   if (unlikely(nexus->flags & NET_F_DEPOSIT)) {
+      rt_source_t *src = get_pseudo_source(get_model(), nexus, SOURCE_DEPOSIT);
+      nexus->flags &= NET_F_DEPOSIT;
+      return value_ptr(nexus, &(src->u.deposit.value));
    }
 
    // If S has no source, then the driving value of S is given by the
@@ -1926,6 +1946,7 @@ static void *driving_value(rt_nexus_t *nexus)
                + nexus->signal->offset + nexus->offset;
 
       case SOURCE_FORCING:
+      case SOURCE_DEPOSIT:
          // An undriven signal that was previously forced
          assert(s->disconnected);
          return nexus_driving(nexus);
@@ -2863,7 +2884,7 @@ static void async_fast_all_drivers(rt_model_t *m, void *arg)
    fast_update_all_drivers(m, signal);
 }
 
-static void async_update_driving(rt_model_t *m, void *arg)
+static void async_force_release(rt_model_t *m, void *arg)
 {
    rt_nexus_t *nexus = arg;
    update_driver(m, nexus, NULL);
@@ -2872,12 +2893,7 @@ static void async_update_driving(rt_model_t *m, void *arg)
 static void async_deposit(rt_model_t *m, void *arg)
 {
    rt_deposit_t *deposit = arg;
-
-   // Force takes priority over deposit
-   if (!(deposit->nexus->flags & NET_F_FORCED))
-      update_driving(m, deposit->nexus, deposit->data);
-
-   free(deposit);
+   update_driving(m, deposit->nexus, driving_value(deposit->nexus));
 }
 
 static void async_transfer_signal(rt_model_t *m, void *arg)
@@ -3164,7 +3180,7 @@ void force_signal(rt_model_t *m, rt_signal_t *s, const void *values,
 
       n->flags |= NET_F_FORCED;
 
-      rt_source_t *src = get_forcing_source(m, n);
+      rt_source_t *src = get_pseudo_source(m, n, SOURCE_FORCING);
       copy_value_ptr(n, &(src->u.forcing), vptr);
 
       deltaq_insert_force_release(m, n);
@@ -3185,10 +3201,9 @@ void release_signal(rt_model_t *m, rt_signal_t *s, int offset, size_t count)
       count -= n->width;
       assert(count >= 0);
 
-      if (n->flags & NET_F_FORCED)
-         n->flags &= ~NET_F_FORCED;
+      n->flags &= ~NET_F_FORCED;
 
-      rt_source_t *src = get_forcing_source(m, n);
+      rt_source_t *src = get_pseudo_source(m, n, SOURCE_FORCING);
       src->disconnected = 1;
 
       deltaq_insert_force_release(m, n);
@@ -3211,11 +3226,12 @@ void deposit_signal(rt_model_t *m, rt_signal_t *s, const void *values,
       count -= n->width;
       assert(count >= 0);
 
-      rt_deposit_t *d = xmalloc_flex(sizeof(rt_deposit_t), n->width, n->size);
-      d->nexus = n;
-      memcpy(d->data, vptr, n->width * n->size);
+      n->flags |= NET_F_DEPOSIT;
 
-      deltaq_insert_deposit(m, d);
+      rt_source_t *src = get_pseudo_source(m, n, SOURCE_DEPOSIT);
+      copy_value_ptr(n, &(src->u.deposit.value), vptr);
+
+      deltaq_insert_deposit(m, &(src->u.deposit));
       vptr += n->width * n->size;
    }
 }

src/rt/rt.h
@@ -59,7 +59,7 @@ typedef enum {
 #define NET_F_CACHE_EVENT  (1 << 2)
 #define NET_F_R_IDENT      (1 << 3)
 // Unused                  (1 << 4)
-// Unused                  (1 << 5)
+#define NET_F_DEPOSIT      (1 << 5)
 #define NET_F_FAST_DRIVER  (1 << 6)
 #define NET_F_EFFECTIVE    (1 << 7)
 typedef uint8_t net_flags_t;

src/rt/structs.h
@@ -95,6 +95,7 @@ typedef enum {
    SOURCE_DRIVER,
    SOURCE_PORT,
    SOURCE_FORCING,
+   SOURCE_DEPOSIT,
 } source_kind_t;
 
 typedef struct {
@@ -122,6 +123,11 @@ typedef struct {
    rt_conv_func_t *conv_func;
 } rt_port_t;
 
+typedef struct {
+   rt_nexus_t *nexus;
+   rt_value_t  value;
+} rt_deposit_t;
+
 typedef struct _rt_source {
    rt_source_t    *chain_input;
    rt_source_t    *chain_output;
@@ -133,6 +139,7 @@ typedef struct _rt_source {
       rt_port_t    port;
       rt_driver_t  driver;
       rt_value_t   forcing;
+      rt_deposit_t deposit;
    } u;
 } rt_source_t;
 

commit 07791c71f368ceae43aa2fe2181fad61a2dbfc69

Fix typo introduced by b52c576. Issue #857

src/rt/model.c
@@ -2004,7 +2004,7 @@ static void *calculate_driving_value(rt_model_t *m, rt_nexus_t *n)
    // for the signal of which S is a subelement, as appropriate.
    if (unlikely(n->flags & NET_F_DEPOSIT)) {
       rt_source_t *src = get_pseudo_source(m, n, SOURCE_DEPOSIT);
-      n->flags &= NET_F_DEPOSIT;
+      n->flags &= ~NET_F_DEPOSIT;
       return value_ptr(n, &(src->u.deposit.value));
    }
 

src/vhpi/vhpi-model.c
@@ -1614,13 +1614,13 @@ vhpiHandleT vhpi_register_cb(vhpiCbDataT *cb_data_p, int32_t flags)
    cb->data   = *cb_data_p;
 
    if (cb->Reason == vhpiCbAfterDelay) {
-         if (cb->data.time == NULL) {
-            vhpi_error(vhpiError, NULL, "missing time for vhpiCbAfterDelay");
-            goto err;
-         }
+      if (cb->data.time == NULL) {
+         vhpi_error(vhpiError, NULL, "missing time for vhpiCbAfterDelay");
+         goto err;
+      }
 
-         cb->when = vhpi_time_to_native(cb->data.time)
-            + model_now(vhpi_context()->model, NULL);
+      const uint64_t now = model_now(vhpi_context()->model, NULL);
+      cb->when = vhpi_time_to_native(cb->data.time) + now;
    }
    else if (cb->Reason == vhpiCbValueChange && cb->data.value) {
       vhpi_error(vhpiInternal, NULL,

src/vhpi/vhpi-util.c
@@ -331,6 +331,7 @@ bool vhpi_is_repetitive(vhpiEnumT reason)
    case vhpiCbRepLastKnownDeltaCycle:
    case vhpiCbRepNextTimeStep:
    case vhpiCbRepEndOfTimeStep:
+   case vhpiCbRepAfterDelay:
       return true;
    default:
       return false;

test/regress/vhpi12.vhd
@@ -1,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity vhpi12 is
+    port ( d, clk : in std_logic;
+           q : out std_logic );
+end entity;
+
+architecture test of vhpi12 is
+begin
+
+    process (clk) is
+    begin
+        if rising_edge(clk) then
+            q <= d;
+        end if;
+    end process;
+
+end architecture;

test/vhpi/Makemodule.am
@@ -16,7 +16,8 @@ lib_vhpi_test_so_SOURCES = \
 	test/vhpi/issue744.c \
 	test/vhpi/vhpi10.c \
 	test/vhpi/vhpi11.c \
-	test/vhpi/issue762.c
+	test/vhpi/issue762.c \
+	test/vhpi/vhpi12.c
 
 lib_vhpi_test_so_CFLAGS  = $(PIC_FLAG) -I$(top_srcdir)/src/vhpi $(AM_CFLAGS)
 lib_vhpi_test_so_LDFLAGS = -shared $(VHPI_LDFLAGS) $(AM_LDFLAGS)

test/vhpi/vhpi12.c
@@ -1,0 +1,148 @@
+//
+//  Copyright (C) 2024  Nick Gasson
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "vhpi_test.h"
+
+static vhpiHandleT h_sos;
+static vhpiHandleT h_clk;
+static vhpiHandleT h_d;
+static vhpiHandleT h_q;
+static vhpiHandleT h_timeout;
+
+static void register_timeout(void);
+
+static void set_clk(vhpiSmallEnumT bit)
+{
+   vhpiValueT val = {
+      .format = vhpiLogicVal,
+   };
+
+   val.value.enumv = bit;
+   vhpi_put_value(h_clk, &val, vhpiDepositPropagate);
+   check_error();
+}
+
+static vhpiSmallEnumT get_clk(void)
+{
+   vhpiValueT val = {
+      .format = vhpiLogicVal,
+   };
+
+   vhpi_get_value(h_clk, &val);
+   check_error();
+
+   return val.value.enumv;
+}
+
+static void set_d(vhpiSmallEnumT bit)
+{
+   vhpiValueT val = {
+      .format = vhpiLogicVal,
+   };
+
+   val.value.enumv = bit;
+   vhpi_put_value(h_d, &val, vhpiDepositPropagate);
+   check_error();
+}
+
+static vhpiSmallEnumT get_q(void)
+{
+   vhpiValueT val = {
+      .format = vhpiLogicVal,
+   };
+
+   vhpi_get_value(h_q, &val);
+   check_error();
+
+   return val.value.enumv;
+}
+
+static void toggle_clock(const vhpiCbDataT *cb_data)
+{
+   vhpi_printf("toggle clock");
+
+   if (get_clk() == vhpi1) {
+      set_clk(vhpi0);
+
+      vhpiSmallEnumT q = get_q();
+      vhpi_printf("q value %x", q);
+      fail_unless(q == vhpi1);
+   }
+   else
+      set_clk(vhpi1);
+
+   vhpi_release_handle(h_timeout);
+
+   static int rep = 10;
+   if (rep-- > 0)
+      register_timeout();
+}
+
+static void register_timeout(void)
+{
+   vhpiTimeT time = {
+      .low = 1000000
+   };
+
+   vhpiCbDataT delay_cb = {
+      .reason = vhpiCbAfterDelay,
+      .cb_rtn = toggle_clock,
+      .time   = &time
+   };
+   h_timeout = vhpi_register_cb(&delay_cb, vhpiReturnCb);
+   check_error();
+}
+
+static void start_of_sim(const vhpiCbDataT *cb_data)
+{
+   vhpiHandleT root = vhpi_handle(vhpiRootInst, NULL);
+   check_error();
+   fail_if(root == NULL);
+   vhpi_printf("root handle %p", root);
+
+   h_clk = vhpi_handle_by_name("clk", root);
+   check_error();
+   fail_if(h_clk == NULL);
+   vhpi_printf("clk handle %p", h_clk);
+
+   h_d = vhpi_handle_by_name("d", root);
+   check_error();
+   fail_if(h_d == NULL);
+   vhpi_printf("d handle %p", h_d);
+
+   h_q = vhpi_handle_by_name("q", root);
+   check_error();
+   fail_if(h_q == NULL);
+   vhpi_printf("q handle %p", h_q);
+
+   set_clk(vhpi0);
+   set_d(vhpi1);
+
+   register_timeout();
+
+   vhpi_release_handle(h_sos);
+}
+
+void vhpi12_startup(void)
+{
+   vhpiCbDataT cb_data1 = {
+      .reason    = vhpiCbStartOfSimulation,
+      .cb_rtn    = start_of_sim,
+   };
+   h_sos = vhpi_register_cb(&cb_data1, vhpiReturnCb);
+   check_error();
+}

test/vhpi/vhpi_test.c
@@ -40,6 +40,7 @@ static const vhpi_test_t tests[] = {
    { "vhpi10",   vhpi10_startup },
    { "vhpi11",   vhpi11_startup },
    { "issue762", issue762_startup },
+   { "vhpi12",   vhpi12_startup },
    { NULL,       NULL },
 };
 

test/vhpi/vhpi_test.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2023  Nick Gasson
+//  Copyright (C) 2023-2024  Nick Gasson
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -39,6 +39,7 @@ void vhpi8_startup(void);
 void vhpi9_startup(void);
 void vhpi10_startup(void);
 void vhpi11_startup(void);
+void vhpi12_startup(void);
 void issue744_startup(void);
 void issue762_startup(void);
 