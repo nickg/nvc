7bc1c3b42105fe379af77b149a9fa229501b3ba8
Ondrej Ille <ondrej.ille@tropicsqaure.com>
2024-04-20 09:49:29 +0200

    src: Linearize control flow in cover_add_toggle_items_for.

--- src/cov/cov-data.c
+++ src/cov/cov-data.c
@@ -254,92 +254,92 @@ int32_t cover_add_toggle_items_for(cover_data_t *data, type_t type,
    else
       flags |= COV_FLAG_TOGGLE_PORT;
 
-   if (type_is_array(type)) {
-      int t_dims = dimension_of(type);
-      tree_t r = range_of(type, t_dims - curr_dim);
-      int32_t first_item = -1;
-      int64_t low, high;
-
-      if (folded_bounds(r, &low, &high)) {
-         assert(low <= high);
-
-         int64_t first, last, i;
-         int inc;
-
-         if (cover_skip_array_toggle(data, high - low + 1))
-            return -1;
-
-         data->array_depth++;
-#ifdef COVER_DEBUG_SCOPE
-         printf("Added dimension: %d\n", data->array_depth);
-#endif
-
-         switch (tree_subkind(r)) {
-         case RANGE_DOWNTO:
-            i = high;
-            first = high;
-            last = low;
-            inc = -1;
-            break;
-         case RANGE_TO:
-            i = low;
-            first = low;
-            last = high;
-            inc = +1;
-            break;
-         default:
-            fatal_trace("invalid subkind for range: %d", tree_subkind(r));
-         }
-
-         while (1) {
-            char arr_index[16];
-            int32_t tmp = -1;
-            checked_sprintf(arr_index, sizeof(arr_index), "(%"PRIi64")", i);
-            ident_t arr_suffix =
-               ident_prefix(prefix, ident_new(arr_index), '\0');
-
-            // On lowest dimension walk through elements, if elements
-            // are arrays, then start new (nested) recursion.
-            if (curr_dim == 1) {
-               type_t e_type = type_elem(type);
-               if (type_is_array(e_type))
-                  tmp = cover_add_toggle_items_for(data, e_type, where, arr_suffix,
-                                                   dimension_of(e_type));
-               else {
-                  cover_item_t *item = cover_add_item(data,
-                                                      tree_to_object(where),
-                                                      arr_suffix,
-                                                      COV_ITEM_TOGGLE, flags);
-                  if (item)
-                     tmp = item->tag;
-               }
-            }
-            else   // Recurse to lower dimension
-               tmp = cover_add_toggle_items_for(data, type, where, arr_suffix,
-                                                curr_dim - 1);
-
-            if (i == first)
-               first_item = tmp;
-            if (i == last)
-               break;
-
-            i += inc;
-         }
-
-         assert(data->array_depth > 0);
-         data->array_depth--;
-#ifdef COVER_DEBUG_SCOPE
-         printf("Subtracted dimension: %d\n", data->array_depth);
-#endif
-      }
-
-      return first_item;
-   }
-   else {
+   if (!type_is_array(type)) {
       cover_item_t *item = cover_add_item(data, tree_to_object(where),
                                           NULL, COV_ITEM_TOGGLE, flags);
       return item ? item->tag : -1;
    }
+
+   int t_dims = dimension_of(type);
+   tree_t r = range_of(type, t_dims - curr_dim);
+   int64_t low, high;
+
+   if (!folded_bounds(r, &low, &high))
+      return -1;
+
+   assert(low <= high);
+
+   int64_t first, last, i;
+   int32_t first_item = -1;
+   int inc;
+
+   if (cover_skip_array_toggle(data, high - low + 1))
+      return -1;
+
+   data->array_depth++;
+#ifdef COVER_DEBUG_SCOPE
+   printf("Added dimension: %d\n", data->array_depth);
+#endif
+
+   switch (tree_subkind(r)) {
+   case RANGE_DOWNTO:
+      i = high;
+      first = high;
+      last = low;
+      inc = -1;
+      break;
+   case RANGE_TO:
+      i = low;
+      first = low;
+      last = high;
+      inc = +1;
+      break;
+   default:
+      fatal_trace("invalid subkind for range: %d", tree_subkind(r));
+   }
+
+   while (1) {
+      char arr_index[16];
+      int32_t tmp = -1;
+      checked_sprintf(arr_index, sizeof(arr_index), "(%"PRIi64")", i);
+      ident_t arr_suffix =
+         ident_prefix(prefix, ident_new(arr_index), '\0');
+
+      // On lowest dimension walk through elements, if elements
+      // are arrays, then start new (nested) recursion.
+      if (curr_dim == 1) {
+         type_t e_type = type_elem(type);
+         if (type_is_array(e_type))
+            tmp = cover_add_toggle_items_for(data, e_type, where, arr_suffix,
+                                             dimension_of(e_type));
+         else {
+            cover_item_t *item = cover_add_item(data,
+                                                tree_to_object(where),
+                                                arr_suffix,
+                                                COV_ITEM_TOGGLE, flags);
+            if (item)
+               tmp = item->tag;
+         }
+      }
+      else   // Recurse to lower dimension
+         tmp = cover_add_toggle_items_for(data, type, where, arr_suffix,
+                                          curr_dim - 1);
+
+      if (i == first)
+         first_item = tmp;
+      if (i == last)
+         break;
+
+      i += inc;
+   }
+
+   assert(data->array_depth > 0);
+   data->array_depth--;
+#ifdef COVER_DEBUG_SCOPE
+   printf("Subtracted dimension: %d\n", data->array_depth);
+#endif
+
+   return first_item;
 }
 
 cover_item_t *cover_add_item(cover_data_t *data, object_t *obj, ident_t suffix,
