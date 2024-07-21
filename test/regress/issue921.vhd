c777958b72ba1fc23b5338479c94e98adde80873
Nick Gasson <nick@nickg.me.uk>
2024-07-21 19:16:00 +0100

    Also look up external names in port list. Issue #921

--- test/regress/issue921.vhd
+++ test/regress/issue921.vhd
@@ -7,12 +7,15 @@ begin
     b: block is
         generic ( g : integer );
         generic map ( g => 42 );
+        port ( p : integer );
+        port map ( p => 66 );
     begin
     end block;
 
     check: process is
     begin
         assert << constant b.g : integer >> = 42;
+        assert << signal b.p : integer >> = 66;
         wait;
     end process;
 
