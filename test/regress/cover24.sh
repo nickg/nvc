5ddf16a998038bcfbb68f00c95f83e0f27d70c59
ondrej.ille <ondrej.ille@tropicsquare.com>
2024-10-02 12:51:01 +0200

    test: add cover24 test with example report generated per-hierarchy.

---
+++ test/regress/cover24.sh
@@ -0,0 +1,14 @@
+set -xe
+
+pwd
+which nvc
+
+nvc -a $TESTDIR/regress/cover24.vhd -e --cover cover24 -r
+
+# Report per-file
+nvc --cover-report --per-source-file -o html  work/_WORK.COVER24.elab.covdb 2>&1 | tee out.txt
+
+# Adjust output to be work directory relative
+sed -i -e "s/[^ ]*regress\/data\//data\//g" out.txt
+
+diff -u $TESTDIR/regress/gold/cover24.txt out.txt
