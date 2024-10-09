c8ef9aa823d5426d24f972d221d14cac17964dfc
Blebowski <34539154+Blebowski@users.noreply.github.com>
2024-10-09 19:41:08 +0200

    Fix failure on negative range (#1014)
    Fixes #1012

---
+++ test/elab/issue1012.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity issue1012 is
+end entity;
+
+architecture test of issue1012 is
+    signal addr : std_logic_vector(0 downto 0);
+begin
+
+    i_sub_block : block is
+        generic ( WIDTH : natural );
+        generic map ( WIDTH => 0 );
+        port ( addr : in std_logic_vector(WIDTH - 1 downto 0) );
+        port map ( addr  => addr );     -- Error
+    begin
+        -- Should not create toggle coverage for null array
+    end block;
+
+end architecture;
