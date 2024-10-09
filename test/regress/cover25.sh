38aa01fa72e9fabd2383654c23712239d47dccb1
ondrej.ille <ondrej.ille@tropicsquare.com>
2024-10-09 17:01:25 +0200

    test: adapt to remove absolute path from error.

--- test/regress/cover25.sh
+++ test/regress/cover25.sh
@@ -6,6 +6,6 @@ which nvc
 nvc -a $TESTDIR/regress/cover25.vhd -e --cover cover25 -r 2>&1 | tee out.txt
 
 # Adjust output to be work directory relative
-sed -i -e "s/[^ ]*regress\/data\//data\//g" out.txt
+sed -i '2d' out.txt
 
 diff -u $TESTDIR/regress/gold/cover25.txt out.txt
