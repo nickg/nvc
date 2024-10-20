2fcfc7d190db2ab92a2663846eac76dbf01a9e13
Ondrej Ille <34539154+Blebowski@users.noreply.github.com>
2024-10-20 21:16:56 +0200

    Update lib/nvc/cover_pkg-body.vhd
    Co-authored-by: Nick Gasson <nick@nickg.me.uk>

--- lib/nvc/cover_pkg-body.vhd
+++ lib/nvc/cover_pkg-body.vhd
@@ -26,7 +26,6 @@ package body cover_pkg is
                               item      : out t_item_handle;
                               name      : in string;
                               atleast   : in integer;
-                              n_ranges  : in integer;
                               ranges    : in t_item_range_array) is
     begin
         -- Foreign subprogram
