10f0e8201a0a578eb98fef287e42e152324f099f
NikLeberg <39563554+NikLeberg@users.noreply.github.com>
2024-11-02 10:55:00 +0100

    Fix crash on warning of hidden declaration. Issue #1038 (#1048)

--- test/test_parse.c
+++ test/test_parse.c
@@ -6912,6 +6912,21 @@ START_TEST(test_issue991)
 }
 END_TEST
 
+START_TEST(test_issue1038)
+{
+   opt_set_int(OPT_RELAXED, 1);
+   set_standard(STD_08);
+
+   input_from_file(TESTDIR "/parse/issue1038.vhd");
+
+   parse_and_check(T_PACKAGE, T_PACK_BODY);
+
+   fail_unless(parse() == NULL);
+
+   fail_if_errors();
+}
+END_TEST
+
 Suite *get_parse_tests(void)
 {
    Suite *s = suite_create("parse");
@@ -7076,6 +7091,7 @@ Suite *get_parse_tests(void)
    tcase_add_test(tc_core, test_issue961);
    tcase_add_test(tc_core, test_issue977);
    tcase_add_test(tc_core, test_issue991);
+   tcase_add_test(tc_core, test_issue1038);
    suite_add_tcase(s, tc_core);
 
    return s;
