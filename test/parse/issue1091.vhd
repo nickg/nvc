d1814bb31cb14b117d247f33eb09ed6b27ede83f
NikLeberg <niklaus.leuenb+github@gmail.com>
2024-12-09 02:19:08 +0000

    Raise error if reference is used as formal part. Issue #1091

--- test/parse/issue1091.vhd
+++ test/parse/issue1091.vhd
@@ -9,3 +9,20 @@ architecture struct_adderN of adderN is
 begin
     u: adder port map(addend'last_value => addend);
 end;
+
+--------------------------------------------------------------------------------
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+PACKAGE Vital_Memory IS END PACKAGE Vital_Memory;
+PACKAGE BODY Vital_Memory IS
+    PROCEDURE InternalTimingCheck (CONSTANT SetupHigh : IN TIME := 0 ns) IS
+    BEGIN
+    END InternalTimingCheck;
+
+    PROCEDURE VitalMemorySetupHoldCheck (SIGNAL TestSignal : IN time) IS
+    BEGIN
+        InternalTimingCheck (IEEE.SetupHigh => TestSignal);
+    END VitalMemorySetupHoldCheck;
+END PACKAGE BODY Vital_Memory;
