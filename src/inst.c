d9a4dac68d4512420557b34c7fa33fbc89453053
NikLeberg <niklaus.leuenb+github@gmail.com>
2024-12-17 10:28:56 +0000

    Fix crash when new instance with generics is created.

--- src/inst.c
+++ src/inst.c
@@ -170,7 +170,7 @@ static bool instantiate_should_copy_tree(tree_t t, void *__ctx)
       // Make a unique copy of all public declarations in the package
       return decls != NULL && hset_contains(decls, t);
    case T_REF:
-      return tree_kind(tree_ref(t)) == T_GENERIC_DECL;
+      return tree_has_ref(t) && tree_kind(tree_ref(t)) == T_GENERIC_DECL;
    case T_EXTERNAL_NAME:
       return true;    // Relative paths get converted to absolute
    default:
