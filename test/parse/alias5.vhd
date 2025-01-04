9214724b1a672ede6442c103a9f28bd9e0b1d384
NikLeberg <niklaus.leuenb+github@gmail.com>
2025-01-04 05:49:02 +0000

    Fix crash in overload check of invalid alias declarations

--- test/parse/alias5.vhd
+++ test/parse/alias5.vhd
@@ -3,3 +3,9 @@ package test_pkg is
   alias alias_t is type_t;
   constant c : alias_t;
 end package;
+
+package pack is
+  procedure proc;
+  alias proc_alias is x;
+  alias proc_alias is proc [integer];
+end package;
