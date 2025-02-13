fb361e6338e645f73691980674a703ca18c3c85b
Nick Gasson <nick@nickg.me.uk>
2025-02-13 08:21:18 +0000

    Improve test for VHDL-2019 FILE_PATH. Issue #1162

--- test/regress/stdenv3.vhd
+++ test/regress/stdenv3.vhd
@@ -32,16 +32,22 @@ begin
 
     p1: process is
         variable stack : call_path_vector_ptr;
+
+        function ends_with(x, y : string) return boolean is
+        begin
+            return x'length >= y'length and x(x'right - y'length + 1 to x'right) = y;
+        end function;
     begin
         stack := get_call_path;
         assert stack'length = 1;
         assert stack(0).name.all = "P1";
         assert stack(0).file_name.all = "stdenv3.vhd";
-        assert stack(0).file_line = 36;
+        assert stack(0).file_line = 41;
 
         proc3;
 
         report file_path;
+        assert not ends_with(file_path, "stdenv3.vhd");
         wait;
     end process;
 
