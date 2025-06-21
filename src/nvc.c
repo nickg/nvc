2fdae1fb039a99da4c44a450b26c2705614d4beb
Nick Gasson <nick@nickg.me.uk>
2025-06-21 10:54:45 +0100

    Adjust help output

--- src/nvc.c
+++ src/nvc.c
@@ -2208,18 +2208,17 @@ static void usage(void)
 #endif
       { "Coverage report options",
         {
-           { "-o, --output=dir",
-             "Output directory where to store HTML report" },
+           { "-o, --output=dir", "Output directory for HTML report" },
            { "--exclude-file=FILE",
              "Apply exclude file when generating report, see manual for syntax"
            },
            { "--dont-print={covered,uncovered,excluded}",
              "Exclude specified items from coverage report" },
-           { "--item-limit=limit",
-             "Maximal number of items displayed in single report file. "
-             "Default is 5000." },
+           { "--item-limit=LIMIT",
+             "Display at most LIMIT items in a single report file "
+             "(default 5000)" },
            { "--per-file",
-             "Create source file code coverage report."},
+             "Create source file code coverage report." },
         }
       },
       { "Coverage merge options",
