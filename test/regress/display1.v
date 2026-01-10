cb85ba6a47448dc227539c51e554ba685a1e5145
Nick Gasson <nick@nickg.me.uk>
2026-01-10 09:59:00 +0000

    Fix lexer eating double spaces in Verilog strings

--- test/regress/display1.v
+++ test/regress/display1.v
@@ -2,12 +2,15 @@ module display1;
   reg x;
   reg [7:0] y;
   integer   i;
+  reg [39:0] wide;
 
   initial begin
     x = 0;
     y = 42;
     i = -1064739199;
-    $display("hello",,,x,,y);   // hello  0  42
-    $display("i=%d", i);        // i=-1064739199
+    wide = 40'h2389f8924;
+    $display("hello",,,x,,y);   // "hello  0  42"
+    $display("i=%d", i);        // "i=-1064739199"
+    $display("%t | wide=%x - %x  ==> %x", $time, wide, 40'h1, wide - 1);  // "                   0 | wide=02389f8924 - 0000000001  ==> 02389f8923"
   end
 endmodule // display1
