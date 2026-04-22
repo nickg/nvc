d485af5d13110120f6ba91adf0e39c0e8b188190
Nick Gasson <nick@nickg.me.uk>
2026-04-22 21:14:33 +0100

    Fix GNU-ism in issue1444.sh test script
    Fixes #1496

--- test/regress/issue1433.sh
+++ test/regress/issue1433.sh
@@ -5,14 +5,14 @@ export NVC_COLORS=always
 
 ! nvc -a $TESTDIR/parse/seq.vhd 2>stderr 1>stdout
 
-grep -q "parse/seq.vhd:" stderr
+grep -F -q "parse/seq.vhd:" stderr
 
-if ! grep -q "$(printf '\033\[0m')" stderr stdout; then
+if ! grep -F -q "$(printf '\033[0m')" stderr stdout; then
   echo "Color escape sequence missing"
   exit 1
 fi
 
-if grep -q "$(printf '\033]8;;')" stderr stdout; then
+if grep -F -q "$(printf '\033]8;;')" stderr stdout; then
   echo "Link escape sequence present"
   exit 1
 fi
