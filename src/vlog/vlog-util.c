97397e72d6c39cf123679bcbdf246baebeda3229
Nick Gasson <nick@nickg.me.uk>
2026-05-26 20:00:15 +0100

    Implement $itor system function

--- src/vlog/vlog-util.c
+++ src/vlog/vlog-util.c
@@ -341,6 +341,12 @@ bool vlog_is_signed(vlog_node_t v)
       return vlog_is_signed(vlog_value(v));
    case V_BINARY:
       return vlog_is_signed(vlog_left(v)) && vlog_is_signed(vlog_right(v));
+   case V_SYS_FCALL:
+      switch (vlog_subkind(v)) {
+      case V_SYSTF_SIGNED:   return true;
+      case V_SYSTF_UNSIGNED: return false;
+      default:               return false;
+      }
    case V_MIN_TYP_MAX:
       return vlog_is_signed(vlog_value(v));
    default:
