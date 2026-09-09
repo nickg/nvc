dfbf606792d60fd27bfd1e808deb7bc41044da34
Nick Gasson <nick@nickg.me.uk>
2026-09-09 08:24:19 +0100

    Add a check for tcllib in install-osvvm.tcl

--- contrib/install-osvvm.tcl
+++ contrib/install-osvvm.tcl
@@ -1,3 +1,9 @@
+if {[catch {package require fileutil}]} {
+    puts stderr "Error: OSVVM requires the Tcllib fileutil package."
+    puts stderr "       Install Tcllib (for example: apt-get install tcllib) and retry."
+    exit 1
+}
+
 source "Scripts/StartNVC.tcl"
 set ::osvvm::VhdlLibraryDirectory "${::env(NVC_INSTALL_DEST)}"
 set ::osvvm::VhdlLibrarySubdirectory "."
